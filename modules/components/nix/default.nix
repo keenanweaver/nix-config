f184a6e50bd9e6901fd514aa1b32deb5a56832d2
Keenan Weaver <keenanweaver@protonmail.com>
2024-04-16 19:39:19 -0500

    remove cachix

--- modules/components/nix/default.nix
+++ modules/components/nix/default.nix
@@ -37,7 +37,7 @@ in
           "https://nix-gaming.cachix.org"
           "https://nix-community.cachix.org"
           "https://nixpkgs-wayland.cachix.org"
-          "https://yazi.cachix.org"
+          #"https://yazi.cachix.org"
         ];
         trusted-public-keys = [
           "nyx.chaotic.cx-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
@@ -46,7 +46,7 @@ in
           "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
-          "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
+          #"yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
         ];
         trusted-users = [ "${username}" ];
         use-xdg-base-directories = false;
