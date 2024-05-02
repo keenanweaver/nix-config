1dd8252f76d9886973f5681dbd6c27d6f0d5287a
Keenan Weaver <keenanweaver@protonmail.com>
2024-05-02 08:34:43 -0500

    Add steam-deck switch

--- hosts/steamdeck/install-nix.sh
+++ hosts/steamdeck/install-nix.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install steam-deck
 # Home Manager
 nix --extra-experimental-features "nix-command flakes" run home-manager/master -- init
