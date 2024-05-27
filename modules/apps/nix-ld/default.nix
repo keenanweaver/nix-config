1650cd9516cd555b4481e98ba55d8823f5566cc2
Keenan Weaver <keenanweaver@protonmail.com>
2024-05-27 09:46:32 -0500

    add steam-fhs to nix-ld

--- modules/apps/nix-ld/default.nix
+++ modules/apps/nix-ld/default.nix
@@ -2,6 +2,7 @@
   lib,
   config,
   username,
+  pkgs,
   ...
 }:
 let
@@ -16,6 +17,8 @@ in
   config = lib.mkIf cfg.enable {
     programs.nix-ld = {
       enable = true;
+      # https://reddit.com/r/NixOS/comments/1d1nd9l/walking_through_why_precompiled_hello_world/l5vpcdi/?context=3
+      libraries = pkgs.steam-run.fhsenv.args.multiPkgs pkgs;
     };
     home-manager.users.${username} = { };
   };
