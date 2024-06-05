f5224030cc08223986f8d526ad57381de760c881
Keenan Weaver <keenanweaver@protonmail.com>
2024-06-05 15:40:01 -0500

    fix inputs

--- modules/apps/vkbasalt/default.nix
+++ modules/apps/vkbasalt/default.nix
@@ -16,48 +16,50 @@ in
     };
   };
   config = lib.mkIf cfg.enable {
-    home-manager.users.${username} = {
-      home = {
-        file = {
-          vkbasalt-default-config = {
-            enable = true;
-            text = ''
-              depthCapture = off
-              effects = smaa
-              enableOnLaunch = True
-              lutFile = "${reshade}/textures/lut.png"
-              reshadeTexturePath = "${reshade}/textures"
-              reshadeIncludePath = "${reshade}/shaders"
-              smaaEdgeDetection = luma
-              smaaThreshold = 0.05
-              smaaMaxSearchSteps = 32
-              smaaMaxSearchStepsDiag = 16
-              smaaCornerRounding = 25
-              toggleKey = Home
-              # ReShade effects #
-              blending = ${reshade}/shaders/Blending.fxh
-              daltonize = ${reshade}/shaders/Daltonize.fx
-              deband = ${reshade}/shaders/Deband.fx
-              displaydepth = "${reshade}/shaders/DisplayDepth.fx
-              drawtext = ${reshade}/shaders/DrawText.fxh
-              lut = ${reshade}/shaders/LUT.fx
-              macros = ${reshade}/shaders/Macros.fxh
-              reshade = ${reshade}/shaders/ReShade.fxh
-              reshadeui = ${reshade}/shaders/ReShadeUI.fxh
-              tridither = ${reshade}/shaders/TriDither.fxh
-              uimask = ${reshade}/shaders/UIMask.fx 
-            '';
-            target = "${config.xdg.configHome}/vkBasalt/vkBasalt.conf";
+    home-manager.users.${username} =
+      { pkgs, config, ... }:
+      {
+        home = {
+          file = {
+            vkbasalt-default-config = {
+              enable = true;
+              text = ''
+                depthCapture = off
+                effects = smaa
+                enableOnLaunch = True
+                lutFile = "${reshade}/textures/lut.png"
+                reshadeTexturePath = "${reshade}/textures"
+                reshadeIncludePath = "${reshade}/shaders"
+                smaaEdgeDetection = luma
+                smaaThreshold = 0.05
+                smaaMaxSearchSteps = 32
+                smaaMaxSearchStepsDiag = 16
+                smaaCornerRounding = 25
+                toggleKey = Home
+                # ReShade effects #
+                blending = ${reshade}/shaders/Blending.fxh
+                daltonize = ${reshade}/shaders/Daltonize.fx
+                deband = ${reshade}/shaders/Deband.fx
+                displaydepth = "${reshade}/shaders/DisplayDepth.fx
+                drawtext = ${reshade}/shaders/DrawText.fxh
+                lut = ${reshade}/shaders/LUT.fx
+                macros = ${reshade}/shaders/Macros.fxh
+                reshade = ${reshade}/shaders/ReShade.fxh
+                reshadeui = ${reshade}/shaders/ReShadeUI.fxh
+                tridither = ${reshade}/shaders/TriDither.fxh
+                uimask = ${reshade}/shaders/UIMask.fx 
+              '';
+              target = "${config.xdg.configHome}/vkBasalt/vkBasalt.conf";
+            };
+          };
+          packages = with pkgs; [
+            vkbasalt
+            vkbasalt-cli
+          ];
+          sessionVariables = {
+            #ENABLE_VKBASALT = "1";
           };
         };
-        packages = with pkgs; [
-          vkbasalt
-          vkbasalt-cli
-        ];
-        sessionVariables = {
-          #ENABLE_VKBASALT = "1";
-        };
       };
-    };
   };
 }
