dfee640435fe57b91877808e9ed5a334f018fe2a
Keenan Weaver <keenanweaver@protonmail.com>
2024-06-11 09:24:03 -0500

    add flaresolvarr

--- hosts/pi/remorse.nix
+++ hosts/pi/remorse.nix
@@ -41,6 +41,17 @@
   virtualisation = {
     oci-containers = {
       containers = {
+        flare-solvarr = {
+          image = "ghcr.io/flaresolverr/flaresolverr:latest";
+          autoStart = true;
+          ports = [ "127.0.0.1:8191:8191" ];
+          environment = {
+            LOG_LEVEL = "info";
+            LOG_HTML = "false";
+            CAPTCHA_SOLVER = "hcaptcha-solver";
+            TZ = "America/Chicago";
+          };
+        };
         homarr = {
           image = "ghcr.io/ajnart/homarr:latest";
           ports = [ "7575:7575" ];
