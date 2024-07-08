c6195a13c502fa0db862bca4661d043e52ecc017
Keenan Weaver <keenanweaver@protonmail.com>
2024-07-08 10:21:58 -0500

    variables for config

--- modules/apps/mangohud/default.nix
+++ modules/apps/mangohud/default.nix
@@ -6,6 +6,8 @@
 }:
 let
   cfg = config.mangohud;
+  cpu = lib.mkIf (config.networking.hostName == "nixos-desktop") "5800X";
+  gpu = lib.mkIf (config.networking.hostName == "nixos-desktop") "7900XTX";
 in
 {
   options = {
@@ -41,10 +43,12 @@ in
               gpu_load_value=60,90
               gpu_fan
               gpu_voltage
+              gpu_text=${gpu}
               cpu_stats
               cpu_temp
               cpu_power
               cpu_mhz
+              cpu_text=${cpu}
               cpu_load_change
               cpu_load_value=60,90
               core_load_change
@@ -57,7 +61,6 @@ in
               fps_value=30,60
               frametime
               engine_version
-              gpu_name
               vulkan_driver
               wine
               winesync
