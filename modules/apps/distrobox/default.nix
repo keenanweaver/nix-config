1d02bfd8051f800810c51e87a30a25ea66a91a71
Keenan Weaver <keenanweaver@protonmail.com>
2024-07-15 13:50:30 -0500

    shellcheck + corrections

--- modules/apps/distrobox/default.nix
+++ modules/apps/distrobox/default.nix
@@ -49,7 +49,7 @@ in
                   pull=false
                   replace=false
                   start_now=true
-                  container_additional_volumes="/etc/static/profiles/per-user:/etc/profiles/per-user:ro"
+                  container_additional_volumes="/etc/profiles/per-user:/etc/profiles/per-user:ro /etc/static/profiles/per-user:/etc/static/profiles/per-user:ro"
 
                   [bazzite-arch-gaming]
                   home=${config.xdg.dataHome}/distrobox/bazzite-arch-gaming
@@ -58,7 +58,7 @@ in
                   pull=false
                   replace=false
                   start_now=true
-                  container_additional_volumes="/etc/static/profiles/per-user:/etc/profiles/per-user:ro"
+                  container_additional_volumes="/etc/profiles/per-user:/etc/profiles/per-user:ro /etc/static/profiles/per-user:/etc/static/profiles/per-user:ro"                
                 ''
               else
                 ''''
@@ -67,108 +67,112 @@ in
           };
           config-distrobox-bootstrap = {
             enable = true;
-            text = ''
-              chsh -s /bin/bash
-              ## Set paru settings
-              xh -o $XDG_CONFIG_HOME/paru/paru.conf -d https://raw.githubusercontent.com/Morganamilo/paru/master/paru.conf
-              sd '#SudoLoop' 'SudoLoop' $XDG_CONFIG_HOME/paru.conf
-              sd '#CleanAfter' 'CleanAfter' $XDG_CONFIG_HOME/paru/paru.conf
-              sd '#BottomUp' 'BottomUp' $XDG_CONFIG_HOME/paru/paru.conf
-              ## Add Chaotic AUR
-              sudo pacman-key --init
-              sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-              sudo pacman-key --lsign-key 3056513887B78AEB
-              sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-              if ! rg "chaotic" /etc/pacman.conf; then
-                sudo echo -e "[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
-              fi
-              ## Base packages
-              paru -S --needed --noconfirm   \
-              gamemode                       \
-              gamescope-git                  \
-              kdialog                        \
-              lib32-gamemode                 \
-              lib32-libpulse                 \
-              lib32-mangohud                 \
-              lib32-obs-vkcapture-git        \
-              lib32-vkbasalt                 \
-              lib32-vulkan-mesa-layers       \
-              lib32-vulkan-radeon            \
-              lib32-openal                   \
-              lib32-pipewire                 \
-              lib32-pipewire-jack            \
-              libva-mesa-driver              \
-              mangohud                       \
-              vkbasalt                       \
-              vulkan-mesa-layers             \
-              obs-vkcapture-git              \
-              openal                         \
-              pipewire                       \
-              pipewire-pulse                 \
-              pipewire-alsa                  \
-              pipewire-jack                  \
-              wireplumber                    \
-              xdg-desktop-portal-kde
-              ## Install necessary packages
-              paru -Syu --needed --noconfirm \
-              archlinux-keyring              \
-              base-devel                     \
-              yay
-              ## TODO: atuin
-              ## Set up containers
-              if [[ "$HOST" =~ ^bazzite-arch-exodos ]]; then
-                # Games/emulators/tools
-                paru -S --needed --noconfirm \
-                dbgl                         \
-                fluidsynth                   \
-                gwenview                     \
-                innoextract                  \
-                konsole                      \
-                okular
-              elif [[ "$HOST" =~ ^bazzite-arch-gaming ]]; then
-                # Packages that will initially fail
-                paru -S --needed --noconfirm \
-                sm64ex-nightly-git           \
-                #soh                          \
-                #soh-otr-n64_pal_11
-                # Download & place required data files
-                #xh -o $HOME/zeldaoot.zip -d https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Nintendo%2064%20%28BigEndian%29/Legend%20of%20Zelda%2C%20The%20-%20Ocarina%20of%20Time%20%28USA%29%20%28Rev%202%29.zip
-                xh -o $HOME/mario64.zip -d https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Nintendo%2064%20%28BigEndian%29/Super%20Mario%2064%20%28USA%29.zip
-                #ouch d $HOME/zeldaoot.zip -y -d $XDG_CACHE_HOME/paru/clone/soh-otr-n64_pal_11
-                ouch d $HOME/mario64.zip -y -d $XDG_CACHE_HOME/paru/clone/sm64ex-nightly-git
-                #mv $XDG_CACHE_HOME/paru/clone/soh-otr-n64_pal_11/*.z64 $XDG_CACHE_HOME/paru/clone/soh-otr-n64_pal_11/baserom.z64
-                mv $XDG_CACHE_HOME/paru/clone/sm64ex-nightly-git/*.z64 $XDG_CACHE_HOME/paru/clone/sm64ex-nightly-git/baserom.us.z64
-                # Try again
-                paru -S --needed --noconfirm \
-                sm64ex-nightly-git           \
-                #soh                          \
-                #soh-otr-n64_pal_11
-                # Games/emulators/tools
-                paru -S --needed --noconfirm \
-                2s2h-bin                     \
-                aaru                         \
-                archipelagomw-bin            \
-                bizhawk-bin                  \
-                jazzjackrabbit               \
-                jazzjackrabbit2              \
-                lab3d-sdl                    \
-                mesen2-git                   \
-                nuked-sc55                   \
-                openxcom-extended            \
-                perfectdark                  \
-                portproton                   \
-                sonicthehedgehog             \
-                sonicthehedgehog2            \
-                supermarioworld              \
-                xash3d-fwgs-git              \
-                zelda64recomp-bin            \
-                zeldalttp                    \
-                zeldaoot 
-              else 
-                echo "Container hostname not found"
-              fi
-            '';
+            text =
+              ''
+                chsh -s /bin/bash
+                ## Set paru settings
+                xh -o "$XDG_CONFIG_HOME/paru/paru.conf" -d https://raw.githubusercontent.com/Morganamilo/paru/master/paru.conf
+                sd '#SudoLoop' 'SudoLoop' "$XDG_CONFIG_HOME/paru.conf"
+                sd '#CleanAfter' 'CleanAfter' "$XDG_CONFIG_HOME/paru/paru.conf"
+                sd '#BottomUp' 'BottomUp' "$XDG_CONFIG_HOME/paru/paru.conf"
+                ## Add Chaotic AUR
+                sudo pacman-key --init
+                sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+                sudo pacman-key --lsign-key 3056513887B78AEB
+                sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+                if ! rg "chaotic" /etc/pacman.conf; then
+                  echo -e "[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" .. | sudo tee -a /etc/pacman.conf
+                fi
+                ## TODO: atuin
+                ## Set up containers
+              ''
+              + lib.optionalString vars.gaming ''
+                ## Base packages
+                paru -S --needed --noconfirm   \
+                gamemode                       \
+                gamescope-git                  \
+                kdialog                        \
+                lib32-gamemode                 \
+                lib32-libpulse                 \
+                lib32-mangohud                 \
+                lib32-obs-vkcapture-git        \
+                lib32-vkbasalt                 \
+                lib32-vulkan-mesa-layers       \
+                lib32-vulkan-radeon            \
+                lib32-openal                   \
+                lib32-pipewire                 \
+                lib32-pipewire-jack            \
+                libva-mesa-driver              \
+                mangohud                       \
+                vkbasalt                       \
+                vulkan-mesa-layers             \
+                obs-vkcapture-git              \
+                openal                         \
+                pipewire                       \
+                pipewire-pulse                 \
+                pipewire-alsa                  \
+                pipewire-jack                  \
+                wireplumber                    \
+                xdg-desktop-portal-kde
+                ## Install necessary packages
+                paru -Syu --needed --noconfirm \
+                archlinux-keyring              \
+                base-devel                     \
+                yay
+                if [[ "$HOST" =~ ^bazzite-arch-exodos ]]; then
+                  # Games/emulators/tools
+                  paru -S --needed --noconfirm \
+                  dbgl                         \
+                  fluidsynth                   \
+                  gwenview                     \
+                  innoextract                  \
+                  konsole                      \
+                  okular
+                elif [[ "$HOST" =~ ^bazzite-arch-gaming ]]; then
+                  # Packages that will initially fail
+                  paru -S --needed --noconfirm \
+                  sm64ex-nightly-git           \
+                  #soh                          \
+                  #soh-otr-n64_pal_11
+                  # Download & place required data files
+                  #xh -o "$HOME/zeldaoot.zip" -d https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Nintendo%2064%20%28BigEndian%29/Legend%20of%20Zelda%2C%20The%20-%20Ocarina%20of%20Time%20%28USA%29%20%28Rev%202%29.zip
+                  xh -o "$HOME/mario64.zip" -d https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Nintendo%2064%20%28BigEndian%29/Super%20Mario%2064%20%28USA%29.zip
+                  #ouch d "$HOME/zeldaoot.zip" -y -d "$XDG_CACHE_HOME/paru/clone/soh-otr-n64_pal_11"
+                  ouch d "$HOME/mario64.zip" -y -d "$XDG_CACHE_HOME/paru/clone/sm64ex-nightly-git"
+                  #mv "$XDG_CACHE_HOME/paru/clone/soh-otr-n64_pal_11/*.z64" "$XDG_CACHE_HOME/paru/clone/soh-otr-n64_pal_11/baserom.z64"
+                  mv "$XDG_CACHE_HOME/paru/clone/sm64ex-nightly-git/*.z64" "$XDG_CACHE_HOME/paru/clone/sm64ex-nightly-git/baserom.us.z64"
+                  # Try again
+                  paru -S --needed --noconfirm \
+                  sm64ex-nightly-git           \
+                  #soh                          \
+                  #soh-otr-n64_pal_11
+                  # Games/emulators/tools
+                  paru -S --needed --noconfirm \
+                  2s2h-bin                     \
+                  aaru                         \
+                  archipelagomw-bin            \
+                  bizhawk-bin                  \
+                  jazzjackrabbit               \
+                  jazzjackrabbit2              \
+                  lab3d-sdl                    \
+                  mesen2-git                   \
+                  nuked-sc55                   \
+                  openxcom-extended            \
+                  perfectdark                  \
+                  portproton                   \
+                  sonicthehedgehog             \
+                  sonicthehedgehog2            \
+                  supermarioworld              \
+                  xash3d-fwgs-git              \
+                  zelda64recomp-bin            \
+                  zeldalttp                    \
+                  zeldaoot 
+                else 
+                  echo "Container hostname not found"
+                fi
+              '';
             target = "${config.xdg.configHome}/distrobox/bootstrap.sh";
+            executable = true;
           };
           config-distrobox-config-file = {
             enable = true;
