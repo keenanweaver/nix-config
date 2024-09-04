886d5f3cdb2925bf6630b495a0805c4969fa08a3
Keenan Weaver <keenanweaver@protonmail.com>
2024-09-04 13:30:39 -0500

    redo substituters

--- modules/components/nix/default.nix
+++ modules/components/nix/default.nix
@@ -42,19 +42,17 @@ in
         ];
         log-lines = lib.mkDefault 25;
         substituters = [
-          "https://nyx.chaotic.cx"
-          "https://cache.nixos.org"
-          "https://hyprland.cachix.org"
-          "https://nix-gaming.cachix.org"
           "https://nix-community.cachix.org"
+          "https://nyx.chaotic.cx"
+          #"https://hyprland.cachix.org"
+          "https://nix-gaming.cachix.org"
           "https://nixpkgs-wayland.cachix.org"
         ];
         trusted-public-keys = [
-          "nyx.chaotic.cx-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
-          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-          "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "nyx.chaotic.cx-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+          #"hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+          "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
           "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
         ];
         trusted-users = [ "${username}" ];
