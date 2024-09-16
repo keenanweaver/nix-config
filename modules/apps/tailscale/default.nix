e6a40b56c0b8f3273fa22588e4b96b26312d3363
Keenan Weaver <keenanweaver@protonmail.com>
2024-09-15 19:55:14 -0500

    tailscale changes

--- modules/apps/tailscale/default.nix
+++ modules/apps/tailscale/default.nix
@@ -15,7 +15,10 @@ in
     };
   };
   config = lib.mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [ tailscale ];
+    environment.systemPackages = with pkgs; [
+      ethtool
+      tailscale
+    ];
 
     networking.firewall = {
       trustedInterfaces = [ "tailscale0" ];
@@ -27,8 +30,36 @@ in
       tailscale = {
         enable = true;
         openFirewall = true;
+        extraSetFlags = [
+          "--advertise-exit-node"
+        ];
+        useRoutingFeatures = "both";
       };
     };
+
+    # https://github.com/piyoki/nixos-config/blob/6f252c3a21faacdc192d9be7598ee6ab17608db7/system/networking/udp-gro-forwarding.nix#L14
+    systemd.services."udp-gro-forwarding" =
+      let
+        swBin = "/run/current-system/sw/bin";
+      in
+      {
+        description = "UDP Gro Forwarding Service";
+        after = [
+          "network.target"
+          "iptables.service"
+          "ip6tables.service"
+        ];
+        serviceConfig = {
+          ExecStart = "${pkgs.writeShellScript "udp-gro-forwarding" ''
+            set -eux
+            NETDEV=$(${swBin}/ip route show 0/0 | grep 'via' | cut -f5 -d ' ')
+            ${pkgs.ethtool}/bin/ethtool -K $NETDEV rx-udp-gro-forwarding on rx-gro-list off;
+          ''}";
+          Type = "oneshot";
+          Environment = [ "PATH=$PATH:${swBin}" ];
+        };
+        wantedBy = [ "multi-user.target" ];
+      };
 
     home-manager.users.${username} = {
       home.packages = with pkgs; [ ktailctl ];
