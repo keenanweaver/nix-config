c4158f396eef11d4f2b8dc46159635e5323e92c3
Keenan Weaver <keenanweaver@protonmail.com>
2024-09-27 16:24:24 -0500

    mangohud cleanup

--- modules/apps/mangohud/default.nix
+++ modules/apps/mangohud/default.nix
@@ -18,31 +18,6 @@ in
   config = lib.mkIf cfg.enable {
     home-manager.users.${username} =
       { config, pkgs, ... }:
-      let
-        mangohud-get-vars = (
-          pkgs.writeShellApplication {
-            name = "mangohud-get-vars";
-            runtimeInputs = with pkgs; [
-              coreutils
-              findutils
-              glxinfo
-              libnotify
-              ripgrep
-              util-linux
-            ];
-            text = ''
-              # OS
-              rg -w PRETTY_NAME /etc/os-release | cut -d '=' -f2 | tr -d '"' > ${config.xdg.configHome}/MangoHud/vars
-              # Kernel
-              uname -r >> ${config.xdg.configHome}/MangoHud/vars
-              # CPU
-              #lscpu | rg -oP '(?<=Model\sname:).*' | xargs >> ${config.xdg.configHome}/MangoHud/vars
-              # GPU
-              #glxinfo | rg -oP '(?<=Device: )(.*?)(?=\()' | xargs >> ${config.xdg.configHome}/MangoHud/vars
-            '';
-          }
-        );
-      in
       {
         home.file = {
           mangohud-config = {
@@ -87,29 +62,30 @@ in
               show_fps_limit
               resolution
               custom_text=Distro
-              exec=${pkgs.gnused}/bin/sed -n 1p ${config.xdg.configHome}/MangoHud/vars
+              exec=${pkgs.rg}/bin/rg -w PRETTY_NAME /etc/os-release | ${pkgs.coreutils}/bin/cut -d '=' -f2 | ${pkgs.coreutils}/bin/tr -d '"'
+              custom_text=Distrobox
+              exec=${pkgs.bash}/bin/bash -c '[ -n "''${CONTAINER_ID}" ] && echo Yes || echo No'
               custom_text=Kernel
-              exec=${pkgs.gnused}/bin/sed -n 2p ${config.xdg.configHome}/MangoHud/vars
+              exec=${pkgs.coreutils}/bin/uname -r
               text_outline
               position=bottom-right
               no_display
               width=450
               background_alpha=0.5
               toggle_hud=Shift_R
-              legacy_layout=0 # For scripts that rely on the old layout
-              fps_sampling_period=1000
-              font_file=${pkgs.atkinson-hyperlegible}/share/fonts/opentype/AtkinsonHyperlegible-Bold.otf
+              legacy_layout=0 # For scripts that rely on the new layout
+              font_file=${pkgs.lexend}/share/fonts/truetype/lexend/lexend/Lexend-Bold.ttf
               ${lib.optionalString config.catppuccin.enable ''
                 # Catppuccin theming
                 background_color=1e1e2e
                 battery_color=585b70
                 cpu_color=89b4fa
-                cpu_load_color=a6e3a1, f9e2af, f38ba8
+                cpu_load_color=a6e3a1,f9e2af,f38ba8
                 engine_color=cba6f7
-                fps_color=a6e3a1, f9e2af, f38ba8
+                fps_color=f38ba8,f9e2af,a6e3a1
                 frametime_color=a6e3a1
                 gpu_color=a6e3a1
-                gpu_load_color=a6e3a1, f9e2af, f38ba8
+                gpu_load_color=a6e3a1,f9e2af,f38ba8
                 io_color=f9e2af
                 media_player_color=cdd6f4
                 text_color=cdd6f4
@@ -125,28 +101,9 @@ in
             target = "${config.xdg.configHome}/MangoHud/presets.conf";
           };
         };
-        home.packages = with pkgs; [ mangohud-get-vars ];
         programs.mangohud = {
           enable = true;
           package = pkgs.mangohud_git; # Chaotic package
-        };
-        systemd = {
-          user = {
-            services = {
-              "mangohud-get-vars" = {
-                Unit = {
-                  Description = "Writes variables for MangoHud config";
-                };
-                Service = {
-                  ExecStart = "${mangohud-get-vars}/bin/mangohud-get-vars";
-                  Type = "simple";
-                };
-                Install = {
-                  WantedBy = [ "graphical-session.target" ];
-                };
-              };
-            };
-          };
         };
       };
   };
