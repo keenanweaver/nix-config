2088b5b7a4d8baca86af0140a33bbd2f188f4b92
Keenan Weaver <keenanweaver@protonmail.com>
2024-10-05 21:09:40 -0500

    gsr changes

--- modules/apps/gpu-screen-recorder/default.nix
+++ modules/apps/gpu-screen-recorder/default.nix
@@ -2,6 +2,7 @@
   lib,
   config,
   username,
+  pkgs,
   ...
 }:
 let
@@ -16,6 +17,7 @@ in
   config = lib.mkIf cfg.enable {
     programs.gpu-screen-recorder = {
       enable = true;
+      package = (pkgs.callPackage ../../../nix/pkgs/gsr.nix { });
     };
 
     home-manager.users.${username} =
