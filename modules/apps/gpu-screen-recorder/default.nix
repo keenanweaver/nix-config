dd3bff00b60cf25df3ae1a7b2f24b4aaf532fb12
Keenan Weaver <keenanweaver@protonmail.com>
2024-10-17 19:05:36 -0500

    fix gsr

--- modules/apps/gpu-screen-recorder/default.nix
+++ modules/apps/gpu-screen-recorder/default.nix
@@ -2,7 +2,6 @@
   lib,
   config,
   username,
-  pkgs,
   ...
 }:
 let
@@ -18,7 +17,6 @@ in
   config = lib.mkIf cfg.enable {
     programs.gpu-screen-recorder = {
       enable = true;
-      package = pkgs.gsr;
     };
 
     home-manager.users.${username} =
@@ -84,16 +82,15 @@ in
                     "AUDIO_DEVICE_BROWSER=Browser.monitor"
                     "AUDIO_DEVICE_GAME=Game.monitor"
                     "AUDIO_DEVICE_MIC=mono-microphone"
-                    "AUDIO_DEVICE_VOIP=Voice.monitor"
+                    "AUDIO_DEVICE_VOICE=Voice.monitor"
                     "AUDIO_DEVICE_LIVE=Live.monitor"
                     "AUDIO_DEVICE_MUSIC=Music.monitor"
                     "REPLAYDURATION=900"
                     "OUTPUTDIR=${outputDir}"
-                    "MAKEFOLDERS=no"
                     "FPSPPS=no"
                   ];
                   ExecStartPre = "${pkgs.libnotify}/bin/notify-send -t 3000 -u low 'GPU Screen Recorder' 'Replay started' -i com.dec05eba.gpu_screen_recorder -a 'GPU Screen Recorder'";
-                  ExecStart = "${pkgs.gpu-screen-recorder}/bin/gpu-screen-recorder -w $WINDOW -c $CONTAINER -q $QUALITY -f $FRAMERATE -fm $FRAMERATE_MODE -k $CODEC -ac $AUDIO_CODEC -r $REPLAYDURATION -v $FPSPPS -mf $MAKEFOLDERS -a $AUDIO_DEVICE_DEFAUlT -a $AUDIO_DEVICE_GAME -a $AUDIO_DEVICE_MIC -a $AUDIO_DEVICE_BROWSER -a $AUDIO_DEVICE_VOIP -a $AUDIO_DEVICE_MUSIC -a $AUDIO_DEVICE_LIVE -o $OUTPUTDIR";
+                  ExecStart = "${pkgs.gpu-screen-recorder}/bin/gpu-screen-recorder -w $WINDOW -c $CONTAINER -q $QUALITY -f $FRAMERATE -fm $FRAMERATE_MODE -k $CODEC -ac $AUDIO_CODEC -r $REPLAYDURATION -v $FPSPPS -a $AUDIO_DEVICE_DEFAUlT -a $AUDIO_DEVICE_GAME -a $AUDIO_DEVICE_MIC -a $AUDIO_DEVICE_BROWSER -a $AUDIO_DEVICE_VOICE -a $AUDIO_DEVICE_MUSIC -a $AUDIO_DEVICE_LIVE -o $OUTPUTDIR";
                   KillSignal = "SIGINT";
                   Restart = "on-failure";
                   RestartSec = "5";
