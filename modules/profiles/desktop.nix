f38fe9ce277e83eb74566f849c8f5aadecef3c2e
Keenan Weaver <keenanweaver@protonmail.com>
2024-11-07 07:57:30 -0600

    add bluetooth options

--- modules/profiles/desktop.nix
+++ modules/profiles/desktop.nix
@@ -33,6 +33,8 @@ in
         enable = true;
         settings = {
           General = {
+            AutoEnable = false;
+            ControllerMode = "bredr";
             Experimental = "true";
           }; # https://reddit.com/r/NixOS/comments/1aoteqb/keychron_k1_pro_bluetooth_nixos_wkde_install/kq49q9r/?context=3
         };
