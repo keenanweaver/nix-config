f9fa5a02dc2ad31817bf379b9db0b3489546f99c
Keenan Weaver <keenanweaver@protonmail.com>
2024-11-17 07:28:30 -0600

    fix zelda64recomp

--- modules/apps/distrobox/default.nix
+++ modules/apps/distrobox/default.nix
@@ -542,11 +542,11 @@ in
               writeShellScriptBin "${bin-export}" ''
                 export PULSE_SINK="Game"
                 if [ -z "''${CONTAINER_ID}" ]; then
-                  exec "${db-package}/bin/distrobox-enter" -n ${container} -- ${args} '/usr/bin/${bin}' "$@"
+                  gamemoderun "${db-package}/bin/distrobox-enter" -n ${container} -- ${args} '/usr/bin/${bin}' "$@"
                 elif [ -n "''${CONTAINER_ID}" ] && [ "''${CONTAINER_ID}" != "${container}" ]; then
-                  exec distrobox-host-exec '${bin-export}' "$@"
+                  gamemoderun distrobox-host-exec '${bin-export}' "$@"
                 else
-                  exec '/usr/bin/${bin}' "$@"
+                  '/usr/bin/${bin}' "$@"
                 fi
               ''
             )
