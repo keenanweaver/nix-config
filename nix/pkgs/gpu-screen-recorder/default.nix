41068dad0e897a3f09c543fa06f3c060b2e79cbe
Keenan Weaver <keenanweaver@protonmail.com>
2024-11-17 10:57:41 -0600

    bump gsr

--- nix/pkgs/gpu-screen-recorder/default.nix
+++ nix/pkgs/gpu-screen-recorder/default.nix
@@ -26,11 +26,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "gpu-screen-recorder";
-  version = "4.2.6";
+  version = "4.3.0";
 
   src = fetchurl {
     url = "https://dec05eba.com/snapshot/gpu-screen-recorder.git.${finalAttrs.version}.tar.gz";
-    hash = "sha256-3QMBx6Bp2fa5hndJEZHWbkHxyp4cEKW4BYYzk/qFgLk=";
+    hash = "sha256-XTC/+Psub8fo+ALbHkJY+Ht/Frhf5CKvahaidR8J9cQ=";
   };
 
   sourceRoot = ".";
