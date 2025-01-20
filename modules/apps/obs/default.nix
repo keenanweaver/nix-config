6d58fbcfefa0374db19eb11fa8f16aa8be089e80
Keenan Weaver <keenanweaver@protonmail.com>
2025-01-19 19:29:44 -0600

    obs cleanup

--- modules/apps/obs/default.nix
+++ modules/apps/obs/default.nix
@@ -26,13 +26,13 @@ in
       };
       programs.obs-studio = {
         enable = true;
-        plugins = with pkgs; [
-          obs-studio-plugins.input-overlay
-          obs-studio-plugins.looking-glass-obs
-          obs-studio-plugins.obs-gstreamer
-          obs-studio-plugins.obs-pipewire-audio-capture
-          obs-studio-plugins.obs-vaapi
-          obs-studio-plugins.obs-vkcapture
+        plugins = with pkgs.obs-studio-plugins; [
+          input-overlay
+          looking-glass-obs
+          obs-gstreamer
+          obs-pipewire-audio-capture
+          obs-vaapi
+          obs-vkcapture
         ];
       };
       xdg = {
