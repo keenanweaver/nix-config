9c9eb362b31e89cbd516ebdca6771a29675c21cd
Keenan Weaver <keenanweaver@protonmail.com>
2025-01-27 12:38:30 -0600

    ananicy doesn't work

--- modules/apps/gamescope/default.nix
+++ modules/apps/gamescope/default.nix
@@ -20,19 +20,6 @@ in
       package = pkgs.gamescope_git;
       capSysNice = false; # 'true' breaks gamescope for Steam https://github.com/NixOS/nixpkgs/issues/292620#issuecomment-2143529075
     };
-    # Workaround for above https://github.com/NixOS/nixpkgs/issues/351516#issuecomment-2607156591
-    services.ananicy = {
-      enable = true;
-      package = pkgs.ananicy-cpp;
-      rulesProvider = pkgs.ananicy-cpp;
-      extraRules = [
-        {
-          "name" = "gamescope";
-          "nice" = -20;
-        }
-      ];
-    };
-
     home-manager.users.${username} = { };
   };
 }
