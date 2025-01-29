994c6233157c7327b39e5b288808561a100cfdf3
Keenan Weaver <keenanweaver@protonmail.com>
2025-01-29 07:29:28 -0600

    clean up fonts

--- modules/components/fonts/default.nix
+++ modules/components/fonts/default.nix
@@ -7,63 +7,6 @@
 }:
 let
   cfg = config.fonts;
-  apple-fonts = pkgs.stdenv.mkDerivation rec {
-    pname = "apple-fonts";
-    version = "1";
-    pro = pkgs.fetchurl {
-      url = "https://devimages-cdn.apple.com/design/resources/download/SF-Pro.dmg";
-      sha256 = "sha256-IccB0uWWfPCidHYX6sAusuEZX906dVYo8IaqeX7/O88=";
-    };
-    compact = pkgs.fetchurl {
-      url = "https://devimages-cdn.apple.com/design/resources/download/SF-Compact.dmg";
-      sha256 = "sha256-PlraM6SwH8sTxnVBo6Lqt9B6tAZDC//VCPwr/PNcnlk=";
-    };
-    mono = pkgs.fetchurl {
-      url = "https://devimages-cdn.apple.com/design/resources/download/SF-Mono.dmg";
-      sha256 = "sha256-bUoLeOOqzQb5E/ZCzq0cfbSvNO1IhW1xcaLgtV2aeUU=";
-    };
-    ny = pkgs.fetchurl {
-      url = "https://devimages-cdn.apple.com/design/resources/download/NY.dmg";
-      sha256 = "sha256-HC7ttFJswPMm+Lfql49aQzdWR2osjFYHJTdgjtuI+PQ=";
-    };
-    nativeBuildInputs = [ pkgs.p7zip ];
-    sourceRoot = ".";
-    dontUnpack = true;
-    installPhase = ''
-      7z x ${pro}
-      cd SFProFonts 
-      7z x 'SF Pro Fonts.pkg'
-      7z x 'Payload~'
-      mkdir -p $out/fontfiles
-      mv Library/Fonts/* $out/fontfiles
-      cd ..
-
-      7z x ${mono}
-      cd SFMonoFonts
-      7z x 'SF Mono Fonts.pkg'
-      7z x 'Payload~'
-      mv Library/Fonts/* $out/fontfiles
-      cd ..
-
-      7z x ${compact}
-      cd SFCompactFonts
-      7z x 'SF Compact Fonts.pkg'
-      7z x 'Payload~'
-      mv Library/Fonts/* $out/fontfiles
-      cd ..
-
-      7z x ${ny}
-      cd NYFonts
-      7z x 'NY Fonts.pkg'
-      7z x 'Payload~'
-      mv Library/Fonts/* $out/fontfiles
-
-      mkdir -p $out/usr/share/fonts/OTF $out/usr/share/fonts/TTF
-      mv $out/fontfiles/*.otf $out/usr/share/fonts/OTF
-      mv $out/fontfiles/*.ttf $out/usr/share/fonts/TTF
-      rm -rf $out/fontfiles
-    '';
-  };
 in
 {
   options = {
@@ -80,30 +23,16 @@ in
         decompressFonts = true;
       };
       packages = with pkgs; [
-        #atkinson-hyperlegible
-        apple-fonts
-        #b612
-        #barlow
-        #caladea
-        #carlito
         corefonts
-        #fira-sans
-        #font-awesome
-        #geist-font
-        ibm-plex
         inter
-        #lato
-        #lexend
+        lato
         liberation_ttf
         material-design-icons
-        monaspace
-        #nerd-fonts.iosevka
         nerd-fonts.jetbrains-mono
         noto-fonts
         noto-fonts-cjk-sans
         noto-fonts-emoji
         noto-fonts-lgc-plus
-        #poppins
         rubik
         source-han-sans
         source-han-sans-japanese
@@ -117,17 +46,15 @@ in
         defaultFonts = {
           monospace = [
             "JetBrainsMono Nerd Font"
-            "BlexMono Nerd Font"
-            "Noto Color Emoji"
+            "Liberation Mono"
           ];
           sansSerif = [
             "Inter"
-            "IBM Plex Sans"
-            "Noto Color Emoji"
+            "Liberation Sans"
           ];
           serif = [
-            "IBM Plex Serif"
-            "Noto Color Emoji"
+            "Inter"
+            "Liberation Serif"
           ];
           emoji = [ "Noto Color Emoji" ];
         };
@@ -146,8 +73,6 @@ in
         QT_SCALE_FACTOR_ROUNDING_POLICY = "RoundPreferFloor";
       };
     };
-    home-manager.users.${username} = {
-      #fonts.fontconfig.enable = true;
-    };
+    home-manager.users.${username} = { };
   };
 }
