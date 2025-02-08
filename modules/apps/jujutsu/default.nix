ad1d88ea2c09133ad44b3b54403b7d2cc0034d9f
Keenan Weaver <keenanweaver@protonmail.com>
2025-02-08 12:41:47 -0600

    lazyjj fix

--- modules/apps/jujutsu/default.nix
+++ modules/apps/jujutsu/default.nix
@@ -16,7 +16,17 @@ in
   };
   config = lib.mkIf cfg.enable {
     home-manager.users.${username} = {
-      #home.packages = with pkgs; [ lazyjj ];
+      home.packages = with pkgs; [
+        (lazyjj.overrideAttrs {
+          version = "0.4.3-unstable-2025-01-04";
+          src = fetchFromGitHub {
+            owner = "misaelaguayo";
+            repo = "lazyjj";
+            rev = "update-snapshots-jj25";
+            hash = "sha256-XRPDs4TwoDgOypLfv5H4V8YdD04Em8xJ3l+ALRngihQ=";
+          };
+        })
+      ];
       programs.jujutsu = {
         enable = true;
       };
