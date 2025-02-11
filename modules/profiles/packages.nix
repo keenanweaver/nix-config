72946f3a55e8b8bb6a839782d1f974b90d78b846
Keenan Weaver <keenanweaver@protonmail.com>
2025-02-11 16:15:46 -0600

    add shfmt

--- modules/profiles/packages.nix
+++ modules/profiles/packages.nix
@@ -78,6 +78,7 @@ in
             ## Bash ###
             bash-language-server
             shellcheck
+            shfmt
             ### C ###
             clang-tools
             cmake-language-server
