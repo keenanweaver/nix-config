a7a36c0f9780cf0a137e9d2fa188b39901c91a6d
Keenan Weaver <keenanweaver@protonmail.com>
2025-02-17 16:56:03 -0600

    swap to scx_lavd

--- modules/profiles/gaming.nix
+++ modules/profiles/gaming.nix
@@ -310,7 +310,7 @@ in
       scx = {
         enable = true;
         package = pkgs.scx.rustscheds;
-        scheduler = "scx_rusty";
+        scheduler = "scx_lavd";
       };
       udev = {
         extraRules = ''
