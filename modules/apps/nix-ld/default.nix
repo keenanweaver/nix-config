f043a5a83ab1c76fc5c6a875d060bc2d91e2342c
Keenan Weaver <keenanweaver@protonmail.com>
2025-03-11 19:37:06 -0500

    readd nix-ld libs

--- modules/apps/nix-ld/default.nix
+++ modules/apps/nix-ld/default.nix
@@ -2,6 +2,7 @@
   lib,
   config,
   username,
+  pkgs,
   ...
 }:
 let
@@ -16,6 +17,31 @@ in
   config = lib.mkIf cfg.enable {
     programs.nix-ld = {
       enable = true;
+      libraries =
+        with pkgs;
+        (steam-run.args.multiPkgs pkgs)
+        ++ (heroic.args.multiPkgs pkgs)
+        ++ (lutris.args.multiPkgs pkgs)
+        ++ [
+          alsa-lib
+          dbus
+          glibc
+          gtk3
+          icu
+          libcap
+          libxcrypt
+          libGL
+          libdrm
+          libudev0-shim
+          libva
+          mesa
+          networkmanager
+          pkg-config
+          xorg.libX11
+          xorg.libXext
+          udev
+          vulkan-loader
+        ];
     };
     home-manager.users.${username} = { };
   };
