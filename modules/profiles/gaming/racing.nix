e38e8a955914c887220288c7f41378ec677cce64
Keenan Weaver <keenanweaver@protonmail.com>
2025-03-29 07:20:26 -0500

    g920

--- modules/profiles/gaming/racing.nix
+++ modules/profiles/gaming/racing.nix
@@ -25,9 +25,8 @@ in
     };
     environment = {
       etc = {
-        "usb_modeswitch.d/046d:c261" = {
+        logitech-g920 = {
           text = ''
-            # Logitech G920 Racing Wheel
             DefaultVendor=046d
             DefaultProduct=c261
             MessageEndpoint=01
@@ -35,6 +34,7 @@ in
             TargetClass=0x03
             MessageContent="0f00010142"
           '';
+          target = "usb_modeswitch.d/046d:c261";
         };
       };
       systemPackages = with pkgs; [
