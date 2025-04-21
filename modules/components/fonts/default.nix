a038c925cce45c21df951f7e533d34af5bea2964
Keenan Weaver <keenanweaver@protonmail.com>
2025-04-21 17:03:13 -0500

    font cleanup

--- modules/components/fonts/default.nix
+++ modules/components/fonts/default.nix
@@ -46,7 +46,6 @@ in
           ];
           serif = [
             "Liberation Serif"
-            "Adwaita Sans"
           ];
         };
         subpixel.rgba = "rgb";
@@ -54,7 +53,7 @@ in
     };
     environment = {
       sessionVariables = {
-        #https://www.reddit.com/r/linux_gaming/comments/16lwgnj/comment/k1536zb/?utm_source=reddit&utm_medium=web2x&context=3
+        # https://www.reddit.com/r/linux_gaming/comments/16lwgnj/comment/k1536zb/?utm_source=reddit&utm_medium=web2x&context=3
         FREETYPE_PROPERTIES = "cff:no-stem-darkening=0 autofitter:no-stem-darkening=0";
         # https://reddit.com/r/kde/comments/1bjgajv/fractional_scaling_still_seems_to_look_worse_than/kvshkoz/?context=3
         QT_SCALE_FACTOR_ROUNDING_POLICY = "RoundPreferFloor";
