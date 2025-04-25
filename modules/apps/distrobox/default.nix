6e5d9f7f9e1bd47ea7fdaa47dfd3312b7a247817
Keenan Weaver <keenanweaver@protonmail.com>
2025-04-25 06:27:38 -0500

    script logic

--- modules/apps/distrobox/default.nix
+++ modules/apps/distrobox/default.nix
@@ -69,7 +69,7 @@ in
                   sd '#BottomUp' 'BottomUp' "$XDG_CONFIG_HOME/paru/paru.conf"
                 fi
                 ## Add Chaotic AUR
-                if grep -L "chaotic" /etc/pacman.conf; then
+                if ! grep -q "chaotic" /etc/pacman.conf; then
                   sudo pacman-key --init
                   sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
                   sudo pacman-key --lsign-key 3056513887B78AEB
