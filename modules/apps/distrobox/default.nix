8af6909e6f3aca9ea2fa89f6d20abc7a4b06c6d4
Keenan Weaver <keenanweaver@protonmail.com>
2025-04-24 22:29:03 -0500

    distrobox script cleanup

--- modules/apps/distrobox/default.nix
+++ modules/apps/distrobox/default.nix
@@ -62,27 +62,25 @@ in
             ++ lib.optionals cfg.gaming [
               (writeShellScriptBin "bootstrap-distrobox" ''
                 ## Set paru settings
-                mkdir -p $XDG_CONFIG_HOME/paru
-                xh -o "$XDG_CONFIG_HOME/paru/paru.conf" -d https://raw.githubusercontent.com/Morganamilo/paru/master/paru.conf
+                mkdir -p "$XDG_CONFIG_HOME"/paru
+                wget -P "$XDG_CONFIG_HOME"/paru https://raw.githubusercontent.com/Morganamilo/paru/master/paru.conf
                 sd '#SudoLoop' 'SudoLoop' "$XDG_CONFIG_HOME/paru/paru.conf"
                 sd '#CleanAfter' 'CleanAfter' "$XDG_CONFIG_HOME/paru/paru.conf"
                 sd '#BottomUp' 'BottomUp' "$XDG_CONFIG_HOME/paru/paru.conf"
-                ## Add Chaotic AUR
-                sudo pacman-key --init
-                sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-                sudo pacman-key --lsign-key 3056513887B78AEB
-                sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-                if ! rg "chaotic" /etc/pacman.conf; then
+                if [[ ! grep "chaotic" /etc/pacman.conf ]]; then
+                  ## Add Chaotic AUR
+                  sudo pacman-key --init
+                  sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+                  sudo pacman-key --lsign-key 3056513887B78AEB
+                  sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
                   echo -e "[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
                 fi
                 ## Set up containers
                 ${lib.optionalString vars.gaming ''
                   ## Base packages
                   paru -Syu --needed --noconfirm \
-                  gamemode                       \
                   kdialog                        \
                   konsole                        \
-                  lib32-gamemode                 \
                   lib32-libpulse                 \
                   lib32-mangohud                 \
                   lib32-vkbasalt                 \
@@ -92,7 +90,6 @@ in
                   lib32-pipewire-jack            \
                   libva-mesa-driver              \
                   mangohud                       \
-                  vkbasalt                       \
                   openal                         \
                   parui                          \
                   pipewire                       \
@@ -100,6 +97,7 @@ in
                   pipewire-alsa                  \
                   pipewire-jack                  \
                   ttf-jetbrains-mono-nerd        \
+                  vkbasalt                       \
                   wireplumber                    \
                   xdg-desktop-portal-kde
                   ## Install necessary packages
