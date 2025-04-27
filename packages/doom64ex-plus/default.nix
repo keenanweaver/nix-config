98d0b13633bea8d2946a24263b641ed497536e75
Keenan Weaver <keenanweaver@protonmail.com>
2025-04-27 14:39:11 -0500

    add libglu back

--- packages/doom64ex-plus/default.nix
+++ packages/doom64ex-plus/default.nix
@@ -5,6 +5,7 @@
   sdl3,
   fluidsynth,
   libGL,
+  libGLU,
   libpng,
   zlib,
   pkg-config,
@@ -36,6 +37,7 @@ stdenv.mkDerivation (finalAttrs: {
   buildInputs = [
     fluidsynth
     libGL
+    libGLU
     libpng
     sdl3
     zlib
