a0340050114f690db80c82e2685bc28a00818ca1
Keenan Weaver <keenanweaver@protonmail.com>
2025-05-02 08:44:44 -0500

    add zerotier lan discovery route

--- modules/apps/zerotier/default.nix
+++ modules/apps/zerotier/default.nix
@@ -5,6 +5,7 @@
 }:
 let
   cfg = config.zerotier;
+  adapter = "ztmosnophz";
 in
 {
   options = {
@@ -13,10 +14,26 @@ in
     };
   };
   config = lib.mkIf cfg.enable {
-    # See here for additional notes https://github.com/gomaaz/Zerotier_Gaming_Fix
     networking = {
       firewall = {
         allowedUDPPorts = [ 9993 ];
+      };
+      # https://github.com/gomaaz/Zerotier_Gaming_Fix#-considerations-for-gaming-with-linux-and-mac-friends
+      # https://discuss.zerotier.com/t/lan-game-server-not-visible-through-zerotier-local-lan-works/12166/7
+      interfaces = {
+        ${adapter} = {
+          ipv4 = {
+            routes = [
+              {
+                address = "255.255.255.255";
+                prefixLength = 32;
+                options = {
+                  dev = "${adapter}";
+                };
+              }
+            ];
+          };
+        };
       };
     };
     services = {
