5ddfa8e3cf94c72f0bbc2f8b8688a0af8092be8d
Keenan Weaver <keenanweaver@protonmail.com>
2025-05-05 16:59:41 -0500

    add myrient rclone

--- hosts/pi/regret.nix
+++ hosts/pi/regret.nix
@@ -46,5 +46,63 @@
     };
   };
 
-  home-manager.users.${username} = { pkgs, ... }: { };
+  home-manager.users.${username} =
+    { pkgs, lib, ... }:
+    {
+      systemd.user.services = {
+        rclone-myrient-nointro = {
+          Unit = {
+            Description = "Download No-Intro from Myrient";
+          };
+          Service = {
+            Type = "oneshot";
+            ExecStart = lib.getExe (
+              pkgs.writeShellApplication {
+                name = "rclone-myrient-nointro";
+                runtimeInputs = with pkgs; [
+                  rclone
+                ];
+                text =
+                  let
+                    rcloneOpts = {
+                      command = "copy";
+                      source = "myrient:/files/No-Intro/";
+                      destination = "/mnt/crusader/Games/Backups/Myrient/No-Intro/";
+                      args = "-vv --filter-from ${rcloneOpts.filter}";
+                      filter = pkgs.writeText "rclone-myrient-nointro-filter" ''
+                        - CD-ROM*/*
+                        - DVD-ROM*/*
+                        - DVD-VIDEO*/*
+                        - Google*/*
+                        - HD DVD*/*
+                        - IBM*/*
+                        - Microsoft - Xbox*/*
+                        - Mobile*/*
+                        - Nintendo - Misc*/*
+                        - Nintendo - New*/*
+                        - Nintendo - Nintendo 3DS*/*
+                        - Nintendo - SDKs*/*
+                        - Nintendo - Wallpapers*/*
+                        - Nintendo - Wii*/*
+                        - Nintendo - amiibo*/*
+                        - Non-Redump*/*
+                        - Ouya*/*
+                        - Sony*/*
+                        - Source Code*/*
+                        - Unofficial*/*
+                        - Various*/*
+                        - Video CD*/*
+                        - Web*/*
+                      '';
+                    };
+                  in
+                  ''
+                    rclone ${rcloneOpts.command} ${rcloneOpts.source} ${rcloneOpts.destination} ${rcloneOpts.args}
+                  '';
+              }
+            );
+          };
+        };
+      };
+    };
 }
