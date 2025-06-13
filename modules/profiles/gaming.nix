41e2cdd8f50b9bc5ce3aa4a1d70ae7fdaced1eaa
Keenan Weaver <keenanweaver@protonmail.com>
2025-06-13 09:01:26 -0500

    add hdr script

--- modules/profiles/gaming.nix
+++ modules/profiles/gaming.nix
@@ -498,135 +498,161 @@ in
       {
 
         home = {
-          file = {
-            desktop-entry-dxvk =
-              let
-                configFile = pkgs.fetchurl {
-                  url = "https://raw.githubusercontent.com/doitsujin/dxvk/master/dxvk.conf";
-                  hash = "sha256-OydD9rHfPQlsKs+889mQ6DJ14aBePdQ/RWvTiEMQij4=";
+          file =
+            let
+              primaryscreen = "DP-1";
+            in
+            {
+              desktop-entry-dxvk =
+                let
+                  configFile = pkgs.fetchurl {
+                    url = "https://raw.githubusercontent.com/doitsujin/dxvk/master/dxvk.conf";
+                    hash = "sha256-OydD9rHfPQlsKs+889mQ6DJ14aBePdQ/RWvTiEMQij4=";
+                  };
+                in
+                {
+                  enable = true;
+                  text = ''
+                    [Desktop Entry]
+                    Comment=Create a new DXVK config from template
+                    Icon=text-plain
+                    Name=DXVK Config...
+                    Type=Link
+                    URL[$e]=file:${configFile}
+                  '';
+                  target = "${config.xdg.dataHome}/templates/dxvk.desktop";
                 };
-              in
-              {
-                enable = true;
-                text = ''
-                  [Desktop Entry]
-                  Comment=Create a new DXVK config from template
-                  Icon=text-plain
-                  Name=DXVK Config...
-                  Type=Link
-                  URL[$e]=file:${configFile}
-                '';
-                target = "${config.xdg.dataHome}/templates/dxvk.desktop";
-              };
-            desktop-entry-mangohud =
-              let
-                configFile = pkgs.fetchurl {
-                  url = "https://raw.githubusercontent.com/flightlessmango/MangoHud/master/data/MangoHud.conf";
-                  hash = "sha256-hAZePm8o5/55IlSghWKhBJBi63JtKJQzGYDUn69u1oM=";
+              desktop-entry-mangohud =
+                let
+                  configFile = pkgs.fetchurl {
+                    url = "https://raw.githubusercontent.com/flightlessmango/MangoHud/master/data/MangoHud.conf";
+                    hash = "sha256-hAZePm8o5/55IlSghWKhBJBi63JtKJQzGYDUn69u1oM=";
+                  };
+                in
+                {
+                  enable = true;
+                  text = ''
+                    [Desktop Entry]
+                    Comment=Create a new MangoHud config from template
+                    Icon=io.github.flightlessmango.mangohud
+                    Name=MangoHud Config...
+                    Type=Link
+                    URL[$e]=file:${configFile}
+                  '';
+                  target = "${config.xdg.dataHome}/templates/mangohud.desktop";
                 };
-              in
-              {
-                enable = true;
-                text = ''
-                  [Desktop Entry]
-                  Comment=Create a new MangoHud config from template
-                  Icon=io.github.flightlessmango.mangohud
-                  Name=MangoHud Config...
-                  Type=Link
-                  URL[$e]=file:${configFile}
-                '';
-                target = "${config.xdg.dataHome}/templates/mangohud.desktop";
-              };
-            desktop-entry-vkBasalt =
-              let
-                configFile = pkgs.fetchurl {
-                  url = "https://raw.githubusercontent.com/DadSchoorse/vkBasalt/master/config/vkBasalt.conf";
-                  hash = "sha256-IN/Kuc17EZfzRoo8af1XoBX2/48/bCdyOxw/Tl463Mg=";
+              desktop-entry-vkBasalt =
+                let
+                  configFile = pkgs.fetchurl {
+                    url = "https://raw.githubusercontent.com/DadSchoorse/vkBasalt/master/config/vkBasalt.conf";
+                    hash = "sha256-IN/Kuc17EZfzRoo8af1XoBX2/48/bCdyOxw/Tl463Mg=";
+                  };
+                in
+                {
+                  enable = true;
+                  text = ''
+                    [Desktop Entry]
+                    Comment=Create a new vkBasalt config from template
+                    Icon=text-plain
+                    Name=vkBasalt Config...
+                    Type=Link
+                    URL[$e]=file:${configFile}
+                  '';
+                  target = "${config.xdg.dataHome}/templates/vkBasalt.desktop";
                 };
-              in
-              {
+              roms-mt32-exodos = {
                 enable = true;
-                text = ''
-                  [Desktop Entry]
-                  Comment=Create a new vkBasalt config from template
-                  Icon=text-plain
-                  Name=vkBasalt Config...
-                  Type=Link
-                  URL[$e]=file:${configFile}
-                '';
-                target = "${config.xdg.dataHome}/templates/vkBasalt.desktop";
+                source = config.lib.file.mkOutOfStoreSymlink "${inputs.nonfree}/Music/roland";
+                target = "${config.xdg.configHome}/dosbox/mt32-roms";
               };
-            roms-mt32-exodos = {
-              enable = true;
-              source = config.lib.file.mkOutOfStoreSymlink "${inputs.nonfree}/Music/roland";
-              target = "${config.xdg.configHome}/dosbox/mt32-roms";
-            };
-            vrr-off = {
-              enable = true;
-              source =
-                with pkgs;
-                lib.getExe (writeShellApplication {
-                  name = "vrr-off";
-                  runtimeInputs = [
-                    kdePackages.libkscreen
-                  ];
-                  text =
-                    let
-                      primaryscreen = "DP-1";
-                    in
-                    ''
+              screen-hdr-off = {
+                enable = true;
+                source =
+                  with pkgs;
+                  lib.getExe (writeShellApplication {
+                    name = "hdr-off";
+                    runtimeInputs = [
+                      kdePackages.libkscreen
+                    ];
+                    text = ''
+                      kscreen-doctor output.${primaryscreen}.hdr.disable output.${primaryscreen}.wcg.disable
+                    '';
+                  });
+                target = "${config.home.homeDirectory}/Games/hdr-off.sh";
+              };
+              screen-hdr-on = {
+                enable = true;
+                source =
+                  with pkgs;
+                  lib.getExe (writeShellApplication {
+                    name = "hdr-on";
+                    runtimeInputs = [
+                      kdePackages.libkscreen
+                    ];
+                    text = ''
+                      kscreen-doctor output.${primaryscreen}.hdr.enable output.${primaryscreen}.wcg.enable
+                    '';
+                  });
+                target = "${config.home.homeDirectory}/Games/hdr-on.sh";
+              };
+              screen-vrr-off = {
+                enable = true;
+                source =
+                  with pkgs;
+                  lib.getExe (writeShellApplication {
+                    name = "vrr-off";
+                    runtimeInputs = [
+                      kdePackages.libkscreen
+                    ];
+                    text = ''
                       kscreen-doctor output.${primaryscreen}.vrrpolicy.never
                     '';
-                });
-              target = "${config.home.homeDirectory}/Games/vrr-off.sh";
-            };
-            vrr-on = {
-              enable = true;
-              source =
-                with pkgs;
-                lib.getExe (writeShellApplication {
-                  name = "vrr-on";
-                  runtimeInputs = [
-                    kdePackages.libkscreen
-                  ];
-                  text =
-                    let
-                      primaryscreen = "DP-1";
-                    in
-                    ''
+                  });
+                target = "${config.home.homeDirectory}/Games/vrr-off.sh";
+              };
+              screen-vrr-on = {
+                enable = true;
+                source =
+                  with pkgs;
+                  lib.getExe (writeShellApplication {
+                    name = "vrr-on";
+                    runtimeInputs = [
+                      kdePackages.libkscreen
+                    ];
+                    text = ''
                       kscreen-doctor output.${primaryscreen}.vrrpolicy.automatic
                     '';
-                });
-              target = "${config.home.homeDirectory}/Games/vrr-on.sh";
-            };
-            wine-controller-proton = {
-              # https://selfmadepenguin.wordpress.com/2024/02/14/how-i-solved-my-gamecontroller-problems/
-              # Import with: wine start regedit.exe /home/keenan/.wine/controller-proton.reg
-              enable = true;
-              text = ''
-                Windows Registry Editor Version 5.00
+                  });
+                target = "${config.home.homeDirectory}/Games/vrr-on.sh";
+              };
+              wine-controller-proton = {
+                # https://selfmadepenguin.wordpress.com/2024/02/14/how-i-solved-my-gamecontroller-problems/
+                # Import with: wine start regedit.exe /home/keenan/.wine/controller-proton.reg
+                enable = true;
+                text = ''
+                  Windows Registry Editor Version 5.00
 
-                [HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\winebus]
-                "DisableHidraw"=dword:00000001
-                "Enable SDL"=dword:00000001
-              '';
-              target = "${config.home.homeDirectory}/Games/wine-controller.reg";
-            };
-            wine-mouse-acceleration = {
-              # https://reddit.com/r/linux_gaming/comments/1hs1685/windows_mouse_acceleration_seems_to_be_enabled_in/
-              # Import with: wine start regedit.exe /home/keenan/.wine/mouse-acceleration-proton.reg
-              enable = true;
-              text = ''
-                Windows Registry Editor Version 5.00
+                  [HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\winebus]
+                  "DisableHidraw"=dword:00000001
+                  "Enable SDL"=dword:00000001
+                '';
+                target = "${config.home.homeDirectory}/Games/wine-controller.reg";
+              };
+              wine-mouse-acceleration = {
+                # https://reddit.com/r/linux_gaming/comments/1hs1685/windows_mouse_acceleration_seems_to_be_enabled_in/
+                # Import with: wine start regedit.exe /home/keenan/.wine/mouse-acceleration-proton.reg
+                enable = true;
+                text = ''
+                  Windows Registry Editor Version 5.00
 
-                [HKEY_CURRENT_USER\Control Panel\Mouse]
-                "MouseSpeed"="0"
-                "MouseThreshold1"="0"
-                "MouseThreshold2"="0"
-              '';
-              target = "${config.home.homeDirectory}/Games/wine-mouse-acceleration.reg";
+                  [HKEY_CURRENT_USER\Control Panel\Mouse]
+                  "MouseSpeed"="0"
+                  "MouseThreshold1"="0"
+                  "MouseThreshold2"="0"
+                '';
+                target = "${config.home.homeDirectory}/Games/wine-mouse-acceleration.reg";
+              };
             };
-          };
           packages =
             with pkgs;
             [
