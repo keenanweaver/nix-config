b5028c483d4da3bd711d8aa49fcedc57bfabeb78
Keenan Weaver <keenanweaver@protonmail.com>
2025-08-14 09:39:19 -0500

    add fsr4

--- modules/apps/steam/default.nix
+++ modules/apps/steam/default.nix
@@ -8,6 +8,48 @@
 }:
 let
   cfg = config.steam;
+
+  # FSR4
+  fsrVersion = "67D435F7d97000";
+  fsrDll = pkgs.fetchurl {
+    url = "https://download.amd.com/dir/bin/amdxcffx64.dll/${fsrVersion}/amdxcffx64.dll";
+    sha256 = "sha256-OuTTllFAwQjzKJXbRhV7Ma15AgFo1U+EHFYqH9/EqVw="; # fix hash
+    curlOpts = "--referer https://support.amd.com";
+  };
+
+  proton-ge-bin-fsr4 =
+    (pkgs.proton-ge-bin.override { steamDisplayName = "GE-Proton FSR"; }).overrideAttrs
+      (old: {
+        installPhase = ''
+          runHook preInstall
+
+          # Make it impossible to add to an environment. You should use the appropriate NixOS option.
+          # Also leave some breadcrumbs in the file.
+          echo "${old.pname} should not be installed into environments. Please use programs.steam.extraCompatPackages instead." > $out
+
+          mkdir $steamcompattool
+          cp -a $src/* $steamcompattool
+          chmod -R +w $steamcompattool
+
+          rm $steamcompattool/compatibilitytool.vdf
+          cp $src/compatibilitytool.vdf $steamcompattool
+
+          runHook postInstall
+        '';
+
+        postInstall = ''
+          mkdir -p $steamcompattool/files/lib/wine/amdprop
+          cp ${fsrDll} $steamcompattool/files/lib/wine/amdprop/amdxcffx64.dll
+          echo "${fsrVersion}" > $steamcompattool/files/lib/wine/amdprop/amdxcffx64_version
+        '';
+
+        preFixup = (old.preFixup or "") + ''
+          substituteInPlace "$steamcompattool/proton" \
+            --replace-fail 'if not version_match:' '# if not version_match:' \
+            --replace-fail 'with open(g_proton.lib_dir + "wine/amdprop/amdxcffx64_version", "w") as file:' '# with open(g_proton.lib_dir + "wine/amdprop/amdxcffx64_version", "w") as file:' \
+            --replace-fail 'file.write(versions[1] + "\n")' '# file.write(versions[1] + "\n")'
+        '';
+      });
 in
 {
   options.steam = {
@@ -39,6 +81,7 @@ in
           OBS_VKCAPTURE = true;
           PROTON_ENABLE_WAYLAND = true;
           PROTON_ENABLE_HDR = true;
+          PROTON_FSR4_UPGRADE = true;
           PROTON_USE_NTSYNC = true;
           PROTON_USE_WOW64 = true;
           PULSE_SINK = "Game";
@@ -53,6 +96,7 @@ in
         luxtorpeda
         inputs.chaotic.packages.${system}.proton-cachyos
         proton-ge-bin
+        proton-ge-bin-fsr4
       ];
       localNetworkGameTransfers.openFirewall = true;
       protontricks.enable = true;
@@ -76,6 +120,11 @@ in
                 unShaderBackgroundProcessingThreads 8
               '';
               target = "${config.xdg.dataHome}/Steam/steam_dev.cfg";
+            };
+            wine-links-protonge-fsr = {
+              enable = true;
+              source = config.lib.file.mkOutOfStoreSymlink "${proton-ge-bin-fsr4.steamcompattool}";
+              target = "${config.xdg.dataHome}/Steam/compatibilitytools.d/GE-Proton10-bin-fsr";
             };
           };
           packages = with pkgs; [
