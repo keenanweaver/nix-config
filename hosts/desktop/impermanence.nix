d203e6237c33c1cb5445c76ce9792c22e01a199d
Keenan Weaver <keenanweaver@protonmail.com>
2025-09-15 15:57:59 -0500

    clean up impermanence

--- hosts/desktop/impermanence.nix
+++ hosts/desktop/impermanence.nix
@@ -4,27 +4,18 @@
       "/persist" = {
         hideMounts = true;
         directories = [
+          "/etc/coolercontrol"
           "/etc/mullvad-vpn"
           "/etc/NetworkManager/system-connections"
-          "/etc/opensnitchd"
           "/etc/secureboot"
           "/etc/wireguard"
           "/var/db/sudo/lectured"
-          "/var/lib/AccountsService"
-          "/var/lib/bluetooth"
-          "/var/lib/clamav"
-          "/var/lib/docker"
-          "/var/lib/flatpak"
-          "/var/lib/libvirt"
-          "/var/lib/nixos"
-          "/var/lib/systemd/coredump"
-          "/var/lib/tailscale"
-          "/var/lib/upower"
-          "/var/lib/zerotier-one"
+          "/var/lib"
           "/var/log"
         ];
         files = [
           "/etc/machine-id"
+          "/etc/nix/id_rsa"
           "/etc/ssh/ssh_host_ed25519_key"
           "/etc/ssh/ssh_host_ed25519_key.pub"
           "/etc/ssh/ssh_host_rsa_key"
