bde4901b6858d8562cc76928fa6667d345099468
Keenan Weaver <keenanweaver@protonmail.com>
2025-10-04 12:33:35 -0500

    redo gsr module

--- modules/apps/gpu-screen-recorder/default.nix
+++ modules/apps/gpu-screen-recorder/default.nix
@@ -5,7 +5,6 @@
   username,
   ...
 }:
-
 let
   cfg = config.gsr;
   package = cfg.package.override {
@@ -13,97 +12,79 @@ let
   };
 in
 {
-  options = {
-    gsr = {
-      package = lib.mkPackageOption pkgs "gpu-screen-recorder" { };
+  options.gsr = {
+    enable = lib.mkEnableOption "Enable 'gpu-screen-recorder' with setcap wrappers for promptless recording";
+    package = lib.mkPackageOption pkgs "gpu-screen-recorder" { };
+    ui = {
       enable = lib.mkOption {
         type = lib.types.bool;
-        default = false;
-        description = ''
-          Whether to install gpu-screen-recorder and generate setcap
-          wrappers for promptless recording.
-        '';
+        default = cfg.enable;
+        description = "Enable 'gpu-screen-recorder-ui' with setcap wrappers for global hotkeys";
       };
-      ui = {
-        package = lib.mkPackageOption pkgs "gpu-screen-recorder-ui" { };
-        enable = lib.mkOption {
-          type = lib.types.bool;
-          default = false;
-          description = ''
-            Whether to install gpu-screen-recorder-ui and generate setcap
-            wrappers for global hotkeys.
-          '';
-        };
-      };
-      defaultAudioDevice = lib.mkOption {
+      package = lib.mkPackageOption pkgs "gpu-screen-recorder-ui" { };
+      saveHotkey = lib.mkOption {
         type = lib.types.str;
+        default = "Meta+Alt+]";
+        description = "Keyboard shortcut for saving GPU Screen Recorder replay";
       };
     };
   };
 
-  config = lib.mkIf cfg.enable {
-    environment.systemPackages = [
-      cfg.package
-      cfg.ui.package
-    ];
-    security.wrappers = {
-      "gsr-global-hotkeys" = {
-        owner = "root";
-        group = "root";
-        capabilities = "cap_setuid+ep";
-        source = lib.getExe' cfg.ui.package "gsr-global-hotkeys";
-      };
-      "gsr-kms-server" = {
+  config = lib.mkMerge [
+    (lib.mkIf cfg.enable {
+      environment.systemPackages = [ cfg.package ];
+
+      security.wrappers.gsr-kms-server = {
         owner = "root";
         group = "root";
         capabilities = "cap_sys_admin+ep";
         source = lib.getExe' package "gsr-kms-server";
       };
-    };
+    })
 
-    home-manager.users.${username} = {
-      home.packages = with pkgs; [
-        (writeShellApplication {
-          name = "gsr-save-replay";
-          runtimeInputs = [
-            killall
-          ];
-          text = ''
-            killall -SIGUSR1 gpu-screen-recorder
-          '';
-        })
-      ];
-      programs.plasma = {
-        hotkeys = {
-          commands = {
-            "gsr-save-replay" = {
-              name = "Save GSR Replay";
-              key = "Meta+Ctrl+|";
-              command = "gsr-save-replay";
-              comment = "Save GPU Screen Recorder replay";
-            };
-          };
-        };
+    (lib.mkIf cfg.ui.enable {
+      environment.systemPackages = [ cfg.ui.package ];
+
+      security.wrappers.gsr-global-hotkeys = {
+        owner = "root";
+        group = "root";
+        capabilities = "cap_setuid+ep";
+        source = lib.getExe' cfg.ui.package "gsr-global-hotkeys";
       };
-      # Using built-in autostart in the GUI will need manual intervention/updating as it results in hanging symlink after some time.
-      systemd.user.services = {
-        "gpu-screen-recorder-ui" = {
-          Unit = {
-            Description = "GPU Screen Recorder UI Service";
-          };
+
+      home-manager.users.${username} = {
+        home.packages = with pkgs; [
+          (writeShellApplication {
+            name = "gsr-save-replay";
+            runtimeInputs = [ killall ];
+            text = ''
+              killall -SIGUSR1 gpu-screen-recorder
+            '';
+          })
+        ];
+
+        programs.plasma.hotkeys.commands.gsr-save-replay = {
+          name = "Save GSR Replay";
+          key = cfg.ui.saveHotkey;
+          command = "gsr-save-replay";
+          comment = "Save GPU Screen Recorder replay";
+        };
+
+        # Using built-in autostart in the GUI will need manual intervention/updating
+        # as it results in hanging symlink after some time.
+        systemd.user.services.gpu-screen-recorder-ui = {
+          Unit.Description = "GPU Screen Recorder UI Service";
           Service = {
-            ExecStart = "${pkgs.gpu-screen-recorder-ui}/bin/gsr-ui launch-daemon";
+            ExecStart = "${lib.getExe' cfg.ui.package "gsr-ui"} launch-daemon";
             KillSignal = "SIGINT";
             Restart = "on-failure";
             RestartSec = 5;
           };
-          Install = {
-            WantedBy = [ "default.target" ];
-          };
+          Install.WantedBy = [ "default.target" ];
         };
       };
-    };
-  };
+    })
+  ];
 
-  meta.maintainers = with lib.maintainers; [ timschumi ];
+  meta.maintainers = with lib.maintainers; [ keenanweaver ];
 }
