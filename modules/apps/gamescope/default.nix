0a085e20032127b6f42d552461aa49462ae7554e
Keenan Weaver <keenanweaver@protonmail.com>
2025-10-21 06:26:07 -0500

    gamescope: add process kill patch

--- modules/apps/gamescope/default.nix
+++ modules/apps/gamescope/default.nix
@@ -22,6 +22,13 @@ in
         final: prev: {
           # https://github.com/ValveSoftware/gamescope/issues/1622
           NIX_CFLAGS_COMPILE = [ "-fno-fast-math" ];
+          patches = prev.patches ++ [
+            # Fix Gamescope not closing https://github.com/ValveSoftware/gamescope/pull/1908
+            (pkgs.fetchpatch {
+              url = "https://github.com/ValveSoftware/gamescope/commit/fa900b0694ffc8b835b91ef47a96ed90ac94823b.patch?full_index=1";
+              hash = "sha256-eIHhgonP6YtSqvZx2B98PT1Ej4/o0pdU+4ubdiBgBM4=";
+            })
+          ];
         }
       );
       capSysNice = false; # 'true' breaks gamescope for Steam https://github.com/NixOS/nixpkgs/issues/292620#issuecomment-2143529075
