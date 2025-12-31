a10e2a6477b35a03a37051306050a5f715c4e3e4
Keenan Weaver <keenanweaver@protonmail.com>
2025-12-31 17:53:15 -0600

    fix proton-em expression

--- packages/proton-em/default.nix
+++ packages/proton-em/default.nix
@@ -3,8 +3,11 @@
   lib,
   proton-ge-bin,
 }:
+let
+  steamDisplayName = "Proton EM";
+in
 (proton-ge-bin.override {
-  steamDisplayName = "Proton-EM";
+  inherit steamDisplayName;
 }).overrideAttrs
   (
     finalAttrs: _: {
@@ -15,6 +18,21 @@
         url = "https://github.com/Etaash-mathamsetty/Proton/releases/download/EM-${finalAttrs.version}/proton-EM-${finalAttrs.version}.tar.xz";
         hash = "sha256-2cE9dydqGXVd67PaIhCtAP51LXt3zOSccGveoEh1AC8=";
       };
+
+      preFixup = ''
+        substituteInPlace "$steamcompattool/compatibilitytool.vdf" \
+          --replace-fail "proton-EM-${finalAttrs.version}" "${steamDisplayName}"
+        substituteInPlace "$steamcompattool/compatibilitytool.vdf" \
+          --replace-fail "-proton" ""
+      '';
+
+      passthru.updateScript = lib.writeScript "update-proton-em" ''
+        #!/usr/bin/env nix-shell
+        #!nix-shell -i bash -p curl jq common-updater-scripts
+        repo="https://api.github.com/repos/Etaash-mathamsetty/Proton/releases"
+        version="$(curl -sL "$repo" | jq 'map(select(.prerelease == false)) | .[0].tag_name' --raw-output)"
+        update-source-version proton-em "$version"
+      '';
 
       meta = {
         inherit (proton-ge-bin.meta)
