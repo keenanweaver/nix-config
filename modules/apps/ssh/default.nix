9829eef5724b7ccbd0299a4bd0eab2aa48a49420
Keenan Weaver <keenanweaver@protonmail.com>
2026-01-01 09:35:25 -0600

    ssh: harden/simplify

--- modules/apps/ssh/default.nix
+++ modules/apps/ssh/default.nix
@@ -18,36 +18,19 @@ in
       enableAskPassword = true;
       startAgent = true;
     };
+    services.fail2ban.enable = true;
     services.openssh = {
       enable = true;
-      listenAddresses = [
-        {
-          addr = "0.0.0.0";
-          port = 6777;
-        }
-      ];
       ports = [ 6777 ];
       settings = {
         AllowUsers = [ "${username}" ];
-        # Allow forwarding ports to everywhere
-        GatewayPorts = "clientspecified";
         KbdInteractiveAuthentication = false;
-        KexAlgorithms = [
-          "sntrup761x25519-sha512@openssh.com"
-          "curve25519-sha256"
-          "curve25519-sha256@libssh.org"
-          #"diffie-hellman-group-exchange-sha256"
-        ];
         PasswordAuthentication = false;
         PermitRootLogin = "no";
-        # Automatically remove stale sockets
-        StreamLocalBindUnlink = "yes";
-        UseDns = true;
-        X11Forwarding = true;
       };
     };
     home-manager.users.${username} =
-      { config, pkgs, ... }:
+      { pkgs, ... }:
       {
         home.packages = with pkgs; [
           lazyssh
@@ -96,11 +79,11 @@ in
             };
             regretpi = {
               hostname = "regretpi";
-              port = 6777;
+              port = 22;
             };
             remorsepi = {
               hostname = "remorsepi";
-              port = 6777;
+              port = 22;
             };
             unifi-CKG2 = {
               hostname = "unifi";
@@ -114,20 +97,6 @@ in
             };
           };
         };
-        xdg.autostart.entries =
-          let
-            desktopEntry = pkgs.makeDesktopItem {
-              name = "ssh-add";
-              desktopName = "ssh-add";
-              exec = "ssh-add -q ${config.home.homeDirectory}/.ssh/id_ed25519";
-              comment = "Run ssh-add";
-              terminal = false;
-              startupNotify = false;
-            };
-          in
-          [
-            "${desktopEntry}/share/applications/${desktopEntry.name}"
-          ];
       };
   };
 }
