41958344e8521a1f0760b63cc29c8e5affda3d41
Keenan Weaver <keenanweaver@protonmail.com>
2026-01-25 20:56:47 -0600

    gamemode: add new icon

--- modules/apps/gamemode/default.nix
+++ modules/apps/gamemode/default.nix
@@ -26,42 +26,49 @@ in
           park_cores = "no";
           pin_cores = "1-7,16-23";
         };
-        custom = {
-          start = lib.getExe (
-            pkgs.writeShellApplication {
-              name = "gamemode-start";
-              runtimeInputs = with pkgs; [
-                kdePackages.libkscreen
-                kdePackages.qttools
-                libnotify
-              ];
-              text = ''
-                notify-send -t 3000 -u low "GameMode" "GameMode started<br>Disabling Night Light" -i input-gamepad -a "GameMode"
+        custom =
+          let
+            icon = pkgs.fetchurl {
+              url = "https://avatars.githubusercontent.com/u/9704713?s=200&v=4";
+              hash = "sha256-P6Q/d+VGiMfkAKIygIDCr4Idb6dCxeW+fBQK35ZELPU=";
+            };
+          in
+          {
+            start = lib.getExe (
+              pkgs.writeShellApplication {
+                name = "gamemode-start";
+                runtimeInputs = with pkgs; [
+                  kdePackages.libkscreen
+                  kdePackages.qttools
+                  libnotify
+                ];
+                text = ''
+                  notify-send -t 3000 -u low "GameMode" "GameMode started<br>Disabling Night Light" -i ${icon} -a "GameMode"
 
-                if [ "$(qdbus org.kde.KWin /org/kde/KWin/NightLight org.kde.KWin.NightLight.running)" = "true" ]; then
-                  qdbus org.kde.kglobalaccel /component/kwin invokeShortcut "Toggle Night Color"
-                fi
-              '';
-            }
-          );
-          end = lib.getExe (
-            pkgs.writeShellApplication {
-              name = "gamemode-end";
-              runtimeInputs = with pkgs; [
-                kdePackages.libkscreen
-                kdePackages.qttools
-                libnotify
-              ];
-              text = ''
-                notify-send -t 3000 -u low "GameMode" "GameMode stopped<br>Enabling Night Light" -i input-gamepad -a "GameMode"
+                  if [ "$(qdbus org.kde.KWin /org/kde/KWin/NightLight org.kde.KWin.NightLight.running)" = "true" ]; then
+                    qdbus org.kde.kglobalaccel /component/kwin invokeShortcut "Toggle Night Color"
+                  fi
+                '';
+              }
+            );
+            end = lib.getExe (
+              pkgs.writeShellApplication {
+                name = "gamemode-end";
+                runtimeInputs = with pkgs; [
+                  kdePackages.libkscreen
+                  kdePackages.qttools
+                  libnotify
+                ];
+                text = ''
+                  notify-send -t 3000 -u low "GameMode" "GameMode stopped<br>Enabling Night Light" -i ${icon} -a "GameMode"
 
-                if [ "$(qdbus org.kde.KWin /org/kde/KWin/NightLight org.kde.KWin.NightLight.running)" = "false" ]; then
-                  qdbus org.kde.kglobalaccel /component/kwin invokeShortcut "Toggle Night Color"
-                fi
-              '';
-            }
-          );
-        };
+                  if [ "$(qdbus org.kde.KWin /org/kde/KWin/NightLight org.kde.KWin.NightLight.running)" = "false" ]; then
+                    qdbus org.kde.kglobalaccel /component/kwin invokeShortcut "Toggle Night Color"
+                  fi
+                '';
+              }
+            );
+          };
       };
     };
     users.users.${username}.extraGroups = [ "gamemode" ];
