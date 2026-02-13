4227d88ccda565847dd4bf032290bdfdeb67a5b2
Keenan Weaver <keenanweaver@protonmail.com>
2026-02-12 20:05:18 -0600

    add v4l2loopback

--- modules/apps/obs/default.nix
+++ modules/apps/obs/default.nix
@@ -27,6 +27,15 @@ in
     };
   };
   config = lib.mkIf cfg.enable {
+    boot = {
+      extraModulePackages = with config.boot.kernelPackages; [
+        v4l2loopback
+      ];
+      kernelModules = [ "v4l2loopback" ];
+      extraModprobeConfig = ''
+        options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+      '';
+    };
     home-manager.users.${username} = {
       home = {
         packages =
