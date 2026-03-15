b788ef4bca9a612537487547bbee587602bce565
Keenan Weaver <keenanweaver@protonmail.com>
2026-03-15 12:31:33 -0500

    game-wrapper: remove wine dlls

--- packages/game-wrapper/default.nix
+++ packages/game-wrapper/default.nix
@@ -6,7 +6,6 @@ pkgs.writeShellApplication {
     PIPEWIRE_NODE = "Game";
     PULSE_SINK = "Game";
     DEFAULT_MESA_LOADER = "zink";
-    DEFAULT_WINE_OVERRIDES = "dinput,dinput8,dxgi,dsound,ddraw";
   };
   runtimeInputs = with pkgs; [
     gamemode
@@ -14,13 +13,6 @@ pkgs.writeShellApplication {
     obs-studio-plugins.obs-vkcapture
   ];
   text = ''
-    if [[ -z "''${WINEDLLOVERRIDES+x}" ]]; then
-      export WINEDLLOVERRIDES="$DEFAULT_WINE_OVERRIDES=n,b"
-    elif [[ -z "$WINEDLLOVERRIDES" ]]; then
-      unset WINEDLLOVERRIDES
-    else
-      export WINEDLLOVERRIDES="$DEFAULT_WINE_OVERRIDES=n,b,$WINEDLLOVERRIDES"
-    fi
     if [[ -z "''${MESA_LOADER_DRIVER_OVERRIDE+x}" ]]; then
       export MESA_LOADER_DRIVER_OVERRIDE="$DEFAULT_MESA_LOADER"
       exec env gamemoderun mangohud "$@"
