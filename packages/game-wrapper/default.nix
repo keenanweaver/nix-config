49a59858bbea86371dfd4db6436822077d849f22
Keenan Weaver <keenanweaver@protonmail.com>
2026-06-14 18:55:28 -0500

    game-wrapper: copy cachyos stuff

--- packages/game-wrapper/default.nix
+++ packages/game-wrapper/default.nix
@@ -2,10 +2,15 @@
 pkgs.writeShellApplication {
   name = "game-wrapper";
   runtimeEnv = {
+    LOW_LATENCY_LAYER = "1";
     OBS_VKCAPTURE = "1";
     PIPEWIRE_NODE = "Game";
     PULSE_SINK = "Game";
+    # zink-run https://wiki.cachyos.org/features/cachyos_settings/#zink-run
     DEFAULT_MESA_LOADER = "zink";
+    DEFAULT_GALLIUM_DRIVER = "zink";
+    DEFAULT_GLX_VENDOR_LIBRARY_NAME = "mesa";
+    DEFAULT_EGL_VENDOR_LIBRARY_FILENAMES = "${pkgs.mesa-git}/share/glvnd/egl_vendor.d/50_mesa.json";
   };
   runtimeInputs = with pkgs; [
     gamemode
@@ -15,9 +20,15 @@ pkgs.writeShellApplication {
   text = ''
     if [[ -z "''${MESA_LOADER_DRIVER_OVERRIDE+x}" ]]; then
       export MESA_LOADER_DRIVER_OVERRIDE="$DEFAULT_MESA_LOADER"
+      export GALLIUM_DRIVER="$DEFAULT_GALLIUM_DRIVER"
+      export __GLX_VENDOR_LIBRARY_NAME="$DEFAULT_GLX_VENDOR_LIBRARY_NAME"
+      export __EGL_VENDOR_LIBRARY_FILENAMES="$DEFAULT_EGL_VENDOR_LIBRARY_FILENAMES"
       exec env gamemoderun mangohud "$@"
     elif [[ -z "$MESA_LOADER_DRIVER_OVERRIDE" ]]; then
       unset MESA_LOADER_DRIVER_OVERRIDE
+      unset GALLIUM_DRIVER
+      unset __GLX_VENDOR_LIBRARY_NAME
+      unset __EGL_VENDOR_LIBRARY_FILENAMES
       unset OBS_VKCAPTURE
       exec env gamemoderun obs-gamecapture mangohud "$@"
     else
