9d8cf299f3a6479c2d8cbb1ce7049915c13b7717
Keenan Weaver <keenanweaver@protonmail.com>
2026-07-20 07:17:32 -0500

    feat(nuked-sc55): disable autostart
    Popping sounds, always playing

--- modules/profiles/gaming/nuked-sc55.nix
+++ modules/profiles/gaming/nuked-sc55.nix
@@ -1,23 +1,24 @@
 {
-  flake.modules.homeManager.gaming-profile = { lib, pkgs, ... }: {
+  flake.modules.homeManager.gaming-profile = { pkgs, ... }: {
     home.packages = [ pkgs.local.nuked-sc55 ];
-
-    xdg.autostart.entries = [
-      (
-        pkgs.makeDesktopItem {
-          categories = [
-            "Audio"
-            "AudioVideo"
-          ];
-          comment = "Roland SC-55 MIDI emulator";
-          desktopName = "Nuked SC-55 (autostart)";
-          exec = "env PIPEWIRE_NODE=MIDI PULSE_SINK=MIDI SDL_APP_NAME=Nuked ${lib.getExe pkgs.local.nuked-sc55} --no-lcd --romset mk1-v1.21";
-          name = "nuked-sc55-autostart";
-          noDisplay = true;
-          terminal = false;
-        }
-        + "/share/applications/nuked-sc55-autostart.desktop"
-      )
-    ];
+    /*
+      xdg.autostart.entries = [
+         (
+           pkgs.makeDesktopItem {
+             categories = [
+               "Audio"
+               "AudioVideo"
+             ];
+             comment = "Roland SC-55 MIDI emulator";
+             desktopName = "Nuked SC-55 (autostart)";
+             exec = "env PIPEWIRE_NODE=MIDI PULSE_SINK=MIDI SDL_APP_NAME=Nuked ${lib.getExe pkgs.local.nuked-sc55} --no-lcd --romset mk1-v1.21";
+             name = "nuked-sc55-autostart";
+             noDisplay = true;
+             terminal = false;
+           }
+           + "/share/applications/nuked-sc55-autostart.desktop"
+         )
+       ];
+    */
   };
 }
