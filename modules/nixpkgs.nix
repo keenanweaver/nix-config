1a492f76bebf0f446f71f2b03aa018ae2847bb9b
Keenan Weaver <keenanweaver@protonmail.com>
2026-07-25 14:10:50 -0500

    feat(flake): add master & nixpkgs-unstable

--- modules/nixpkgs.nix
+++ modules/nixpkgs.nix
@@ -1,20 +1,39 @@
 { inputs, ... }:
 {
+  flake-file.inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-lib.follows = "nixpkgs";
+    nixpkgs-master.url = "github:NixOS/nixpkgs/master";
+    nixpkgs-unstable.url = "github:/nixos/nixpkgs/nixpkgs-unstable";
+  };
+
   flake.modules.nixos.base-profile = {
     nixpkgs.config = {
       allowBroken = false;
       allowUnfree = true;
     };
   };
-  flake-file.inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-lib.follows = "nixpkgs";
-  };
+
   perSystem = { system, ... }: {
     _module.args.pkgs = import inputs.nixpkgs {
       inherit system;
       config.allowUnfree = true;
-      overlays = [ inputs.chaotic.overlays.default ];
+
+      overlays = [
+        inputs.chaotic.overlays.default
+        (final: _prev: {
+          master = import inputs.nixpkgs-master {
+            inherit (final) config;
+            inherit system;
+          };
+        })
+        (final: _prev: {
+          unstable = import inputs.nixpkgs-unstable {
+            inherit (final) config;
+            inherit system;
+          };
+        })
+      ];
     };
   };
 }
