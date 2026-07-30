df20bc9457313aff54c6410deca28f730af0ffcd
Keenan Weaver <keenanweaver@protonmail.com>
2026-07-30 17:59:03 -0500

    test(nixos-desktop): unlink Games2

--- modules/hosts/nixos-desktop/host.nix
+++ modules/hosts/nixos-desktop/host.nix
@@ -182,7 +182,7 @@
           "d /mnt/Games 0755 ${config.my.user} users - -"
           "d /mnt/Games2 0755 ${config.my.user} users - -"
           "L+ /home/${config.my.user}/Games - - - - /mnt/Games"
-          "L+ /home/${config.my.user}/Games2 - - - - /mnt/Games2"
+          #"L+ /home/${config.my.user}/Games2 - - - - /mnt/Games2"
         ];
       };
     };
