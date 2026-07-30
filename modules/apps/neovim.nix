6178760531eca66758b2d20db4fa89594eb70172
Keenan Weaver <keenanweaver@protonmail.com>
2026-07-30 12:13:05 -0500

    chore(flake): cleanup

--- modules/apps/neovim.nix
+++ modules/apps/neovim.nix
@@ -1,268 +1,266 @@
 {
-  flake = {
-    modules = {
-      homeManager = {
-        base-profile =
-          {
-            inputs,
-            lib,
-            pkgs,
-            ...
-          }:
-          {
-            imports = [
-              inputs.lazyvim.homeManagerModules.default
-            ];
+  flake.modules = {
+    homeManager = {
+      base-profile =
+        {
+          inputs,
+          lib,
+          pkgs,
+          ...
+        }:
+        {
+          imports = [
+            inputs.lazyvim.homeManagerModules.default
+          ];
 
-            home.sessionVariables = {
-              EDITOR = lib.mkForce "nvim";
-            };
-
-            programs.lazyvim = {
-              enable = true;
-
-              extraPackages = with pkgs; [
-                bash-language-server
-                clang
-                shellcheck
-                shellharden
-                shfmt
-                docker-compose-language-service
-                dockerfile-language-server
-                nixd
-                nixfmt
-                vscode-langservers-extracted
-                lua-language-server
-                marksman
-                tree-sitter
-                yamllint
-              ];
-
-              extras = {
-                ai = {
-                  claudecode.enable = true;
-                };
-
-                coding = {
-                  blink.enable = true;
-                  mini-snippets.enable = true;
-                  mini-surround.enable = true;
-                  yanky.enable = true;
-                };
-
-                formatting = {
-                  prettier.enable = true;
-                };
-
-                lang = {
-                  cmake.enable = true;
-                  docker.enable = true;
-                  git.enable = true;
-                  json.enable = true;
-                  nix.enable = true;
-                  terraform.enable = true;
-                  toml.enable = true;
-                  yaml.enable = true;
-                };
-
-                linting = {
-                  eslint.enable = true;
-                };
-
-                ui = {
-                  treesitter-context.enable = true;
-                };
-
-                util = {
-                  gh.enable = true;
-                  project.enable = true;
-                  rest.enable = true;
-                };
-              };
-
-              treesitterParsers = with pkgs.vimPlugins.nvim-treesitter-parsers; [
-                awk
-                bash
-                css
-                html
-                ini
-                jq
-                json
-                just
-                lua
-                markdown
-                nix
-                nu
-                python
-                regex
-                terraform
-                toml
-                udev
-                xml
-                xresources
-                yaml
-                zsh
-              ];
-            };
+          home.sessionVariables = {
+            EDITOR = lib.mkForce "nvim";
           };
 
-        nvf =
-          {
-            inputs,
-            lib,
-            pkgs,
-            ...
-          }:
-          {
-            imports = [
-              inputs.nvf.homeManagerModules.default
+          programs.lazyvim = {
+            enable = true;
+
+            extraPackages = with pkgs; [
+              bash-language-server
+              clang
+              shellcheck
+              shellharden
+              shfmt
+              docker-compose-language-service
+              dockerfile-language-server
+              nixd
+              nixfmt
+              vscode-langservers-extracted
+              lua-language-server
+              marksman
+              tree-sitter
+              yamllint
             ];
 
-            home.sessionVariables = {
-              EDITOR = lib.mkForce "nvim";
+            extras = {
+              ai = {
+                claudecode.enable = true;
+              };
+
+              coding = {
+                blink.enable = true;
+                mini-snippets.enable = true;
+                mini-surround.enable = true;
+                yanky.enable = true;
+              };
+
+              formatting = {
+                prettier.enable = true;
+              };
+
+              lang = {
+                cmake.enable = true;
+                docker.enable = true;
+                git.enable = true;
+                json.enable = true;
+                nix.enable = true;
+                terraform.enable = true;
+                toml.enable = true;
+                yaml.enable = true;
+              };
+
+              linting = {
+                eslint.enable = true;
+              };
+
+              ui = {
+                treesitter-context.enable = true;
+              };
+
+              util = {
+                gh.enable = true;
+                project.enable = true;
+                rest.enable = true;
+              };
             };
 
-            programs.nvf = {
-              enable = true;
-              enableManpages = true;
+            treesitterParsers = with pkgs.vimPlugins.nvim-treesitter-parsers; [
+              awk
+              bash
+              css
+              html
+              ini
+              jq
+              json
+              just
+              lua
+              markdown
+              nix
+              nu
+              python
+              regex
+              terraform
+              toml
+              udev
+              xml
+              xresources
+              yaml
+              zsh
+            ];
+          };
+        };
 
-              settings = {
-                vim = {
-                  extraPackages = with pkgs; [
-                    bash-language-server
-                    shellcheck
-                    shellharden
-                    shfmt
-                    docker-compose-language-service
-                    dockerfile-language-server
-                    vscode-langservers-extracted
-                    marksman
-                    yamllint
-                  ];
+      nvf =
+        {
+          inputs,
+          lib,
+          pkgs,
+          ...
+        }:
+        {
+          imports = [
+            inputs.nvf.homeManagerModules.default
+          ];
 
-                  formatter = {
-                    conform-nvim = {
-                      enable = true;
+          home.sessionVariables = {
+            EDITOR = lib.mkForce "nvim";
+          };
 
-                      setupOpts.formatters-by-ft = {
-                        nix = [ "nixfmt" ];
-                      };
+          programs.nvf = {
+            enable = true;
+            enableManpages = true;
+
+            settings = {
+              vim = {
+                extraPackages = with pkgs; [
+                  bash-language-server
+                  shellcheck
+                  shellharden
+                  shfmt
+                  docker-compose-language-service
+                  dockerfile-language-server
+                  vscode-langservers-extracted
+                  marksman
+                  yamllint
+                ];
+
+                formatter = {
+                  conform-nvim = {
+                    enable = true;
+
+                    setupOpts.formatters-by-ft = {
+                      nix = [ "nixfmt" ];
                     };
                   };
+                };
 
-                  git = {
+                git = {
+                  enable = true;
+                };
+
+                keymaps = [
+                  {
+                    action = "<cmd>w<cr><esc>";
+                    desc = "Save File";
+                    key = "<C-s>";
+
+                    mode = [
+                      "i"
+                      "x"
+                      "n"
+                      "s"
+                    ];
+                  }
+                ];
+
+                languages = {
+                  bash = {
                     enable = true;
                   };
 
-                  keymaps = [
-                    {
-                      action = "<cmd>w<cr><esc>";
-                      desc = "Save File";
-                      key = "<C-s>";
+                  enableDAP = true;
+                  enableExtraDiagnostics = true;
+                  enableFormat = true;
+                  enableTreesitter = true;
 
-                      mode = [
-                        "i"
-                        "x"
-                        "n"
-                        "s"
-                      ];
-                    }
-                  ];
-
-                  languages = {
-                    bash = {
-                      enable = true;
-                    };
-
-                    enableDAP = true;
-                    enableExtraDiagnostics = true;
-                    enableFormat = true;
-                    enableTreesitter = true;
-
-                    markdown = {
-                      enable = true;
-
-                      extensions = {
-                        render-markdown-nvim = {
-                          enable = true;
-                        };
-                      };
-                    };
-
-                    nix = {
-                      enable = true;
-                      format.type = "nixfmt";
-                      lsp.server = "nixd";
-                    };
-                  };
-
-                  lazy = {
+                  markdown = {
                     enable = true;
-                  };
 
-                  lsp = {
-                    enable = true;
-                  };
-
-                  maps = {
-                    normal = {
-                      "<leader>e" = {
-                        action = "<cmd>Neotree toggle reveal<cr>";
-                        desc = "Toggle Neotree";
-                      };
-                    };
-                  };
-
-                  preventJunkFiles = true;
-                  statusline.lualine.enable = true;
-                  syntaxHighlighting = true;
-
-                  terminal = {
-                    toggleterm = {
-                      enable = true;
-
-                      lazygit = {
+                    extensions = {
+                      render-markdown-nvim = {
                         enable = true;
                       };
                     };
                   };
 
-                  treesitter = {
+                  nix = {
+                    enable = true;
+                    format.type = "nixfmt";
+                    lsp.server = "nixd";
+                  };
+                };
+
+                lazy = {
+                  enable = true;
+                };
+
+                lsp = {
+                  enable = true;
+                };
+
+                maps = {
+                  normal = {
+                    "<leader>e" = {
+                      action = "<cmd>Neotree toggle reveal<cr>";
+                      desc = "Toggle Neotree";
+                    };
+                  };
+                };
+
+                preventJunkFiles = true;
+                statusline.lualine.enable = true;
+                syntaxHighlighting = true;
+
+                terminal = {
+                  toggleterm = {
                     enable = true;
 
-                    context = {
-                      enable = true;
-                    };
-
-                    fold = true;
-
-                    highlight = {
-                      enable = true;
-                    };
-
-                    indent = {
+                    lazygit = {
                       enable = true;
                     };
                   };
+                };
 
-                  ui = {
-                    borders = {
-                      enable = true;
-                    };
+                treesitter = {
+                  enable = true;
 
-                    breadcrumbs = {
-                      enable = true;
-                      lualine.winbar.alwaysRender = true;
-                      navbuddy.enable = true;
-                    };
-
-                    colorizer.enable = true;
+                  context = {
+                    enable = true;
                   };
+
+                  fold = true;
+
+                  highlight = {
+                    enable = true;
+                  };
+
+                  indent = {
+                    enable = true;
+                  };
+                };
+
+                ui = {
+                  borders = {
+                    enable = true;
+                  };
+
+                  breadcrumbs = {
+                    enable = true;
+                    lualine.winbar.alwaysRender = true;
+                    navbuddy.enable = true;
+                  };
+
+                  colorizer.enable = true;
                 };
               };
             };
           };
-      };
+        };
     };
   };
 
--- modules/profiles/server.nix
+++ modules/profiles/server.nix
@@ -1,36 +1,34 @@
 {
-  flake = {
-    modules = {
-      homeManager.server-profile =
-        { inputs, ... }:
-        {
-          imports = [
-            inputs.nix-podman-stacks.homeModules.nps
-            inputs.quadlet-nix.homeManagerModules.quadlet
-          ];
+  flake.modules = {
+    homeManager.server-profile =
+      { inputs, ... }:
+      {
+        imports = [
+          inputs.nix-podman-stacks.homeModules.nps
+          inputs.quadlet-nix.homeManagerModules.quadlet
+        ];
+      };
+
+    nixos.server-profile =
+      { inputs, config, ... }:
+      {
+        imports = [
+          inputs.quadlet-nix.nixosModules.quadlet
+        ];
+
+        # https://tarow.github.io/nix-podman-stacks/docs/getting-started.html#%E2%9A%99%EF%B8%8F-prerequisites
+        boot.kernel.sysctl."net.ipv4.ip_unprivileged_port_start" = 0;
+
+        users.users.${config.my.user} = {
+          autoSubUidGidRange = true;
+          linger = true;
         };
 
-      nixos.server-profile =
-        { inputs, config, ... }:
-        {
-          imports = [
-            inputs.quadlet-nix.nixosModules.quadlet
-          ];
-
-          # https://tarow.github.io/nix-podman-stacks/docs/getting-started.html#%E2%9A%99%EF%B8%8F-prerequisites
-          boot.kernel.sysctl."net.ipv4.ip_unprivileged_port_start" = 0;
-
-          users.users.${config.my.user} = {
-            autoSubUidGidRange = true;
-            linger = true;
-          };
-
-          virtualisation.quadlet = {
-            enable = true;
-            autoUpdate.enable = true;
-          };
+        virtualisation.quadlet = {
+          enable = true;
+          autoUpdate.enable = true;
         };
-    };
+      };
   };
 
   flake-file.inputs = {
