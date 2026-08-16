05da80023954746e255469ca9e0669ea79933152
Keenan Weaver <keenanweaver@protonmail.com>
2026-08-16 14:05:45 -0500

    fix(gpg): add ssh pin entry

--- modules/apps/bash.nix
+++ modules/apps/bash.nix
@@ -11,6 +11,10 @@
             cd() {
               builtin cd "$@" && lsd -la
             }
+
+            if [[ -n "$SSH_CONNECTION" ]]; then
+              gpg-connect-agent updatestartuptty /bye >/dev/null 2>&1
+            fi
           '';
         };
       };
