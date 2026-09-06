580f8d1f85a5bf29bf17210d369388f54413cc73
Keenan Weaver <keenanweaver@protonmail.com>
2026-09-06 11:04:10 -0500

    feat(renovate): add nixos-unstable watcher script

--- modules/hosts/regret/renovate.nix
+++ modules/hosts/regret/renovate.nix
@@ -20,7 +20,7 @@
           nix
           openssh
         ];
-        schedule = "*-*-* 00/5:00:00";
+        schedule = null;
         settings = {
           autodiscover = false;
           configMigration = true;
@@ -54,6 +54,43 @@
         "renovate/git_signing" = { };
         "renovate/github_access_token" = { };
         "renovate/nonfree_host_rules" = { };
+      };
+      systemd.services.renovate-watch-nixos-unstable = {
+        description = "Trigger renovate when nixpkgs nixos-unstable advances";
+        after = [ "network-online.target" ];
+        wants = [ "network-online.target" ];
+        serviceConfig = {
+          ExecStart = lib.getExe (
+            pkgs.writeShellApplication {
+              name = "renovate-watch-nixos-unstable";
+              runtimeInputs = [
+                config.systemd.package
+                pkgs.git
+              ];
+              text = ''
+                # shellcheck disable=SC2154 # STATE_DIRECTORY is set by systemd's StateDirectory=
+                state_file="$STATE_DIRECTORY/last-sha"
+                current_sha=$(git ls-remote https://github.com/NixOS/nixpkgs.git refs/heads/nixos-unstable | cut -f1)
+                if [ -z "$current_sha" ]; then
+                  echo "failed to resolve nixos-unstable HEAD" >&2
+                  exit 1
+                fi
+                last_sha=""
+                [ -f "$state_file" ] && last_sha=$(cat "$state_file")
+                if [ "$current_sha" != "$last_sha" ]; then
+                  echo "$current_sha" > "$state_file"
+                  echo "nixos-unstable moved: ''${last_sha:-<none>} -> $current_sha; starting renovate"
+                  systemctl start renovate.service
+                else
+                  echo "nixos-unstable unchanged ($current_sha)"
+                fi
+              '';
+            }
+          );
+          StateDirectory = "renovate-watch-nixos-unstable";
+          Type = "oneshot";
+        };
+        startAt = "00,06,12,18:00:00";
       };
     };
 }
